9f9fe7204a50707fb97e48be56542b241694294c
yawei <yawei@729a0f52-b9d0-41be-9dfd-8227063685e9>
2009-04-08 01:17:16 +0000

    Added the conversion model, and finished the transition model
    git-svn-id: svn://cs2955/var/svn/client/trunk@32 729a0f52-b9d0-41be-9dfd-8227063685e9

---
+++ src/modelers/ConversionModel.java
@@ -0,0 +1,38 @@
+package modelers;
+
+import edu.umich.eecs.tac.props.*;
+
+public class ConversionModel extends AbstractModel {
+
+    private UserModel _userModel;
+    private int _maxCapacity;
+    private int _lastFiveDaySales;
+    private double _capacityDiscounter;
+
+    public static final double DIST_CAPACITY_DISCOUNTER = .995;
+    
+    public ConversionModel(UserModel userModel, int maxCapacity, int lastFiveDaySales) {
+    
+        _userModel = userModel;
+        _maxCapacity = maxCapacity;
+        _lastFiveDaySales = lastFiveDaySales;
+
+        int overCapacity = (int)Math.max(lastFiveDaySales - maxCapacity,0);
+        _capacityDiscounter = Math.pow(DIST_CAPACITY_DISCOUNTER,overCapacity);
+
+
+    }
+
+    public double getConversionProbability(UserState state) {
+        double baseline = 0;
+        if(state == UserState.F0) 
+            baseline = .1;
+        else if(state == UserState.F1) 
+            baseline = .2;
+        else if(state == UserState.F2) 
+            baseline = .3;
+
+        return baseline*_capacityDiscounter;
+    }
+
+}
--- src/modelers/UserStateTransitionModel.java
+++ src/modelers/UserStateTransitionModel.java
@@ -3,13 +3,65 @@ package modelers;
 import java.util.Random;
 
 public class UserStateTransitionModel {
-	
+
+    private static boolean _inited = false;
+    private static HashMap<UserStatePair,Double> _transitions;
+
+    class UserStatePair {
+
+        public UserState from;
+        public UserState to;
+
+        public UserStatePair(UserState f, UserState t) {
+            from = f;
+            to = t;
+        }
+
+        public boolean equals(Object o) {
+            UserStatePair other = (UserStatePair)o;
+            return other.from == from && other.to == to;
+        }
+    }
+    
+
+    public static boolean init() {
+        if(_inited) return true;
+
+        _transitions = new HashMap<UserStatePair,Double>();
+        _transitions.put(new UserStatePair(UserState.TandNS,UserState.IS),.01);
+        _transitions.put(new UserStatePair(UserState.TandNS,UserState.TandNS),.99);
+        
+        _transitions.put(new UserStatePair(UserState.IS,UserState.F0),.6);
+        _transitions.put(new UserStatePair(UserState.IS,UserState.F1),.2);
+        _transitions.put(new UserStatePair(UserState.IS,UserState.F2),.05);
+        _transitions.put(new UserStatePair(UserState.IS,UserState.TandNS),.05);
+        _transitions.put(new UserStatePair(UserState.IS,UserState.IS),.2);
+
+        _transitions.put(new UserStatePair(UserState.F0,UserState.F0),.7);
+        _transitions.put(new UserStatePair(UserState.F0,UserState.F1),.2);
+        _transitions.put(new UserStatePair(UserState.F0,UserState.TandNS),.1);
+
+        _transitions.put(new UserStatePair(UserState.F1,UserState.F1),.7);
+        _transitions.put(new UserStatePair(UserState.F1,UserState.F2),.2);
+        _transitions.put(new UserStatePair(UserState.F1,UserState.TandNS),.1);
+        
+        _transitions.put(new UserStatePair(UserState.F2,UserState.F2),.9);
+        _transitions.put(new UserStatePair(UserState.F2,UserState.TandNS),.1);
+
+        _inited = true;
+        return _inited;
+    }
         public enum UserState {F0, F1, F2, TandNS, IS};
         
         public enum QueryType {F0, F1, F2};
 	
 	public double probability(UserState start, UserState end) {
-		// TODO: is this correct?
-		return (new Random()).nextDouble();
+
+            init();
+
+            Double prob = _transitions.get(new UserStatePair(start,end));
+            if(prob != null) return prob;
+            else return 0;
+
 	}
 }
