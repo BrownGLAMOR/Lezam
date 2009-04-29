46e8a23b7a8a75d89e70034770cf76bdb211a143
spucci <spucci@729a0f52-b9d0-41be-9dfd-8227063685e9>
2009-04-29 17:54:59 +0000

    git-svn-id: svn://cs2955/var/svn/client/trunk@115 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/agents/Cheap.java
+++ src/agents/Cheap.java
@@ -1,19 +1,43 @@
 package agents;
 
 import edu.umich.eecs.tac.props.Ad;
+import edu.umich.eecs.tac.props.AdvertiserInfo;
 import edu.umich.eecs.tac.props.BidBundle;
+import edu.umich.eecs.tac.props.PublisherInfo;
 import edu.umich.eecs.tac.props.Query;
+import edu.umich.eecs.tac.props.QueryType;
+import edu.umich.eecs.tac.props.RetailCatalog;
 
 public class Cheap extends AbstractAgent {
 
 	@Override
+	protected void handleAdvertiserInfo(AdvertiserInfo advertiserInfo) {
+		// TODO Auto-generated method stub
+		super.handleAdvertiserInfo(advertiserInfo);
+	}
+	
+	@Override
 	protected BidBundle buildBidBudle() {
 		BidBundle bidBundle = new BidBundle();
+		double distCap = (double) _advertiserInfo.getDistributionCapacity();
+		double distWind = (double) _advertiserInfo.getDistributionWindow();
+		double dailyDist = distCap/distWind;
 		
 		for(Query q : _querySpace) {
-			double queryBid = .4;
-			double queryBudget = 100;
-
+			double queryBid;
+			double queryBudget;
+			if (q.getType() == QueryType.FOCUS_LEVEL_ZERO){
+				queryBid = .8;
+				queryBudget = .8 * (dailyDist + 10);
+			}
+			else if (q.getType() == QueryType.FOCUS_LEVEL_ONE){
+				queryBid = 1;
+				queryBudget = .8 * (dailyDist + 5);
+			}
+			else {
+				queryBid = 2;
+				queryBudget = .8 * (dailyDist - 5);
+			}
 			bidBundle.addQuery(q, queryBid, null);
 			bidBundle.setDailyLimit(q, queryBudget);
 		}
--- src/agents/JESOM2Agent.java
+++ src/agents/JESOM2Agent.java
@@ -19,15 +19,15 @@ import edu.umich.eecs.tac.props.SalesReport;
 
 public class JESOM2Agent extends AbstractAgent {
 	protected JESOM2BidStrategy _bidStrategy;
-	
+
 	protected AdjustConversionPr _adjustConversionPr;
 	protected Hashtable<Query,Double> _baseLineConversion;
-	
+
 	protected UnitsSoldModel _unitsSold;
-	
-	
+
+
 	public JESOM2Agent(){}
-	
+
 	@Override
 	protected void simulationSetup() {}
 
@@ -40,85 +40,105 @@ public class JESOM2Agent extends AbstractAgent {
 		int distributionWindow = _advertiserInfo.getDistributionWindow();
 		double manufacturerBonus = _advertiserInfo.getManufacturerBonus();
 		String manufacturerSpecialty = _advertiserInfo.getManufacturerSpecialty();
-		
+
 		_unitsSold = new UnitsSoldModelMaxWindow(distributionWindow);
 		for(Query q : _queryFocus.get(QueryType.FOCUS_LEVEL_ZERO)) {_baseLineConversion.put(q, 0.1);}
 		for(Query q : _queryFocus.get(QueryType.FOCUS_LEVEL_ONE)) {_baseLineConversion.put(q, 0.2);}
 		for(Query q : _queryFocus.get(QueryType.FOCUS_LEVEL_TWO)) {_baseLineConversion.put(q, 0.3);}
 		Set<Query> componentSpecialty = _queryComponent.get(_advertiserInfo.getComponentSpecialty());
-		
+
 		_adjustConversionPr = new AdjustConversionPr(distributionCapacity, _unitsSold, _baseLineConversion, componentSpecialty);
-		
-		
+
+
 		new ConversionPr(0.10).apply(_queryFocus.get(QueryType.FOCUS_LEVEL_ZERO), _bidStrategy);
 		new ConversionPr(0.20).apply(_queryFocus.get(QueryType.FOCUS_LEVEL_ONE), _bidStrategy);
 		new ConversionPr(0.30).apply(_queryFocus.get(QueryType.FOCUS_LEVEL_TWO), _bidStrategy);
-		
+
 		new ManufacurerBonus(manufacturerBonus).apply(_queryManufacturer.get(manufacturerSpecialty), _bidStrategy);
-		
+
 		Set<Query> F1componentSpecialty = intersect(_queryFocus.get(QueryType.FOCUS_LEVEL_ONE), componentSpecialty);
 		new ConversionPr(0.27).apply(F1componentSpecialty, _bidStrategy);
 		//for(Query q : F1componentSpecialty) {_baseLineConversion.put(q, 0.27);}
-		
+
 		Set<Query> F2componentSpecialty = intersect(_queryFocus.get(QueryType.FOCUS_LEVEL_TWO), componentSpecialty);
 		new ConversionPr(0.39).apply(F2componentSpecialty, _bidStrategy);
 		//for(Query q : F2componentSpecialty) {_baseLineConversion.put(q, 0.39);}
-		
-		
+
+
 		//??? new Targeted().apply(F1componentSpecialty, _bidStrategy);
 		//new Targeted().apply(F2componentSpecialty, _bidStrategy);
-		
+
 		int slice = distributionCapacity/(20*distributionWindow);
 		new SetProperty(JESOM2BidStrategy.WANTED_SALES, slice).apply(_querySpace, _bidStrategy);
-		new SetProperty(JESOM2BidStrategy.WANTED_SALES, 3*slice).apply(_queryManufacturer.get(manufacturerSpecialty), _bidStrategy);
-		
-		new SetProperty(JESOM2BidStrategy.HONESTY_FACTOR, 0.3).apply(_querySpace, _bidStrategy);
-	}
-	
+		new SetProperty(JESOM2BidStrategy.WANTED_SALES, 2*slice).apply(_queryManufacturer.get(manufacturerSpecialty), _bidStrategy);
 
-	
-	
+		new SetProperty(JESOM2BidStrategy.HONESTY_FACTOR, 0.4).apply(_querySpace, _bidStrategy);
+	}
+
+
+
+
 	@Override
 	protected void updateBidStrategy() {
 		QueryReport qr = _queryReports.remove();
-		
-		
+
+
 		SalesReport sr = _salesReports.remove();
 		_unitsSold.updateReport(sr);
-		
+
 		//_adjustConversionPr.apply(_bidStrategy);
-		
+
 		if (qr!=null){
 			for (Query q : _querySpace){//_bidStrategy.getQuerySpace()){
 				double conversionsGot = sr.getConversions(q);//_unitsSold.getYesterday();
 				double wantedSales = _bidStrategy.getProperty(q, JESOM2BidStrategy.WANTED_SALES);
-				
-				//double clicksAim = _bidStrategy.getQuerySpendLimit(q)/_bidStrategy.getQueryBid(q);
+				double honestyFactor = _bidStrategy.getProperty(q, JESOM2BidStrategy.HONESTY_FACTOR);
+				double conversionRevenue = _bidStrategy.getProperty(q, JESOM2BidStrategy.CONVERSION_REVENUE);
+				double conversionPr = _bidStrategy.getProperty(q, JESOM2BidStrategy.CONVERSION_PR);
+
 				if (conversionsGot < wantedSales){
-					double honestyFactor = _bidStrategy.getProperty(q, JESOM2BidStrategy.HONESTY_FACTOR);
-					_bidStrategy.setProperty(q, JESOM2BidStrategy.HONESTY_FACTOR, honestyFactor*1.14 + .01);
-					_bidStrategy.setProperty(q, JESOM2BidStrategy.WANTED_SALES, wantedSales*.91);
-					
-					//_bidStrategy.setQueryBudget(q, _bidStrategy.getQueryBid(q)*clicksAim);
+					// Didn't get enough sales, but had a good position
+					// -> raise other sales, lower this query's wanted sales
+					if (qr.getPosition(q) < 3){
+						for (Query qu : _querySpace){
+							if (qu != q){
+								_bidStrategy.setProperty(q, JESOM2BidStrategy.WANTED_SALES, wantedSales*1.05);
+							}
+						}
+						_bidStrategy.setProperty(q, JESOM2BidStrategy.WANTED_SALES, wantedSales*.7);
+					}
+					// Not enough sales, bad position
+					// -> raise bid
+					else {
+						_bidStrategy.setProperty(q, JESOM2BidStrategy.HONESTY_FACTOR, honestyFactor*1.2 + .1);
+					}
 				}
 				else {
-					double conversionRevenue = _bidStrategy.getProperty(q, JESOM2BidStrategy.CONVERSION_REVENUE);
-					double conversionPr = _bidStrategy.getProperty(q, JESOM2BidStrategy.CONVERSION_PR);
-					double cpc = qr.getCPC(q)+.01;
-					//just in case the CPC is 0.0
-					
-					double newHonesty = cpc / (conversionRevenue * conversionPr);
-					
-					_bidStrategy.setProperty(q, JESOM2BidStrategy.HONESTY_FACTOR, newHonesty);
-					_bidStrategy.setProperty(q, JESOM2BidStrategy.WANTED_SALES, wantedSales*1.1);
+					// Have enough sales, and our slot is quite low
+					// -> increase desired sales in this query, lower for others
+					if (qr.getPosition(q) > 3 || qr.getCPC(q) < .2){
+						for (Query qu : _querySpace){
+							if (qu != q){
+								_bidStrategy.setProperty(q, JESOM2BidStrategy.WANTED_SALES, wantedSales*.95);
+							}
+						}
+						_bidStrategy.setProperty(q, JESOM2BidStrategy.WANTED_SALES, wantedSales*1.15);
+					}
+					// Have enough sales, and our slot is high
+					// -> lower bid
+					else{
+						double cpc = qr.getCPC(q)-.01;
+						double newHonesty = cpc / (conversionRevenue * conversionPr);
+						_bidStrategy.setProperty(q, JESOM2BidStrategy.HONESTY_FACTOR, newHonesty);
+					}
 				}
 			}
 		}
-		
-		
+
+
 	}
-	
-	
+
+
 	@Override
 	protected BidBundle buildBidBudle(){
 		System.out.println("**********");
@@ -127,5 +147,5 @@ public class JESOM2Agent extends AbstractAgent {
 		return _bidStrategy.buildBidBundle();
 	}
 
-	
+
 }
