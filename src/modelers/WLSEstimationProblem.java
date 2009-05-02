40b57dec0ce218abdd54a944e120367cc86ea0b8
jberg <jberg@729a0f52-b9d0-41be-9dfd-8227063685e9>
2009-05-02 18:34:01 +0000

    git-svn-id: svn://cs2955/var/svn/client/trunk@132 729a0f52-b9d0-41be-9dfd-8227063685e9

---
+++ src/modelers/WLSEstimationProblem.java
@@ -0,0 +1,27 @@
+package modelers;
+
+import org.apache.commons.math.estimation.EstimatedParameter;
+import org.apache.commons.math.estimation.EstimationProblem;
+import org.apache.commons.math.estimation.WeightedMeasurement;
+
+public class WLSEstimationProblem implements EstimationProblem {
+
+	@Override
+	public EstimatedParameter[] getAllParameters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WeightedMeasurement[] getMeasurements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EstimatedParameter[] getUnboundParameters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
