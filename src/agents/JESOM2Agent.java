0ce720bc3f148119ccb43822ccab8832e23d296d
mbarrows <mbarrows@729a0f52-b9d0-41be-9dfd-8227063685e9>
2009-05-22 16:02:10 +0000

    git-svn-id: svn://cs2955/var/svn/client/trunk@196 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/agents/JESOM2Agent.java
+++ src/agents/JESOM2Agent.java
@@ -23,6 +23,31 @@ public class JESOM2Agent extends AbstractAgent {
 
 	protected UnitsSoldModel _unitsSold;
 
+	/*
+	 * Commented by Max. All comments made by spucci are marked with "(spucci)"
+	 * 
+	 * Simplifying assumptions:
+	 * 
+	 * - CPC is close to bid
+	 * - conversion probability is close to what it would be with no informational searchers
+	 * - the 2 assumptions above sort of cancel each other out
+	 * 
+	 * 
+	 * 
+	 * Constants:
+	 * 
+	 * - reinvestment caps
+	 * 
+	 * 
+	 * 
+	 * Models:
+	 * 
+	 * - ConversionPrModel;
+	 * - ConversionPrModelNoIS;
+	 * - UnitsSoldModel; (also in: DistributionCap.java)
+	 * - UnitsSoldModelMean;
+	 * 
+	 */
 
 	public JESOM2Agent(){}
 
@@ -40,34 +65,34 @@ public class JESOM2Agent extends AbstractAgent {
 		String manufacturerSpecialty = _advertiserInfo.getManufacturerSpecialty();
 
 		_unitsSold = new UnitsSoldModelMaxWindow(distributionWindow);
-		for(Query q : _queryFocus.get(QueryType.FOCUS_LEVEL_ZERO)) {_baseLineConversion.put(q, 0.1);}
-		for(Query q : _queryFocus.get(QueryType.FOCUS_LEVEL_ONE)) {_baseLineConversion.put(q, 0.2);}
-		for(Query q : _queryFocus.get(QueryType.FOCUS_LEVEL_TWO)) {_baseLineConversion.put(q, 0.3);}
+		for(Query q : _queryFocus.get(QueryType.FOCUS_LEVEL_ZERO)) {_baseLineConversion.put(q, 0.1);}  // constant set by game server info
+		for(Query q : _queryFocus.get(QueryType.FOCUS_LEVEL_ONE)) {_baseLineConversion.put(q, 0.2);}  // constant set by game server info
+		for(Query q : _queryFocus.get(QueryType.FOCUS_LEVEL_TWO)) {_baseLineConversion.put(q, 0.3);}  // constant set by game server info
 		Set<Query> componentSpecialty = _queryComponent.get(_advertiserInfo.getComponentSpecialty());
 
-		new ConversionPr(0.10).apply(_queryFocus.get(QueryType.FOCUS_LEVEL_ZERO), _bidStrategy);
-		new ConversionPr(0.20).apply(_queryFocus.get(QueryType.FOCUS_LEVEL_ONE), _bidStrategy);
-		new ConversionPr(0.30).apply(_queryFocus.get(QueryType.FOCUS_LEVEL_TWO), _bidStrategy);
+		new ConversionPr(0.10).apply(_queryFocus.get(QueryType.FOCUS_LEVEL_ZERO), _bidStrategy); // constant set by game server info
+		new ConversionPr(0.20).apply(_queryFocus.get(QueryType.FOCUS_LEVEL_ONE), _bidStrategy); // constant set by game server info
+		new ConversionPr(0.30).apply(_queryFocus.get(QueryType.FOCUS_LEVEL_TWO), _bidStrategy); // constant set by game server info
 
 		new ManufacurerBonus(manufacturerBonus).apply(_queryManufacturer.get(manufacturerSpecialty), _bidStrategy);
 
 		Set<Query> F1componentSpecialty = intersect(_queryFocus.get(QueryType.FOCUS_LEVEL_ONE), componentSpecialty);
-		new ConversionPr(0.27).apply(F1componentSpecialty, _bidStrategy);
-		//for(Query q : F1componentSpecialty) {_baseLineConversion.put(q, 0.27);}
+		new ConversionPr(0.27).apply(F1componentSpecialty, _bidStrategy); // constant set by game server info
+		//for(Query q : F1componentSpecialty) {_baseLineConversion.put(q, 0.27);} (spucci)
 
 		Set<Query> F2componentSpecialty = intersect(_queryFocus.get(QueryType.FOCUS_LEVEL_TWO), componentSpecialty);
-		new ConversionPr(0.39).apply(F2componentSpecialty, _bidStrategy);
-		//for(Query q : F2componentSpecialty) {_baseLineConversion.put(q, 0.39);}
+		new ConversionPr(0.39).apply(F2componentSpecialty, _bidStrategy); // constant set by game server info
+		//for(Query q : F2componentSpecialty) {_baseLineConversion.put(q, 0.39);} (spucci)
 
 
 		//??? new Targeted().apply(F1componentSpecialty, _bidStrategy);
 		//new Targeted().apply(F2componentSpecialty, _bidStrategy);
 
 		int slice = distributionCapacity/(20*distributionWindow);
-		new SetProperty(JESOM2BidStrategy.WANTED_SALES, slice).apply(_querySpace, _bidStrategy);
-		new SetProperty(JESOM2BidStrategy.WANTED_SALES, 2*slice).apply(_queryManufacturer.get(manufacturerSpecialty), _bidStrategy);
+		new SetProperty(JESOM2BidStrategy.WANTED_SALES, slice).apply(_querySpace, _bidStrategy); //constant to be entered by user
+		new SetProperty(JESOM2BidStrategy.WANTED_SALES, 2*slice).apply(_queryManufacturer.get(manufacturerSpecialty), _bidStrategy); //constant to be entered by user
 
-		new SetProperty(JESOM2BidStrategy.HONESTY_FACTOR, 0.4).apply(_querySpace, _bidStrategy);
+		new SetProperty(JESOM2BidStrategy.HONESTY_FACTOR, 0.4).apply(_querySpace, _bidStrategy); //constant to be entered by user
 	}
 
 
@@ -81,38 +106,38 @@ public class JESOM2Agent extends AbstractAgent {
 		SalesReport sr = _salesReports.remove();
 		_unitsSold.updateReport(sr);
 
-		//_adjustConversionPr.apply(_bidStrategy);
+		//_adjustConversionPr.apply(_bidStrategy); (spucci)
 
 		if (qr!=null){
-			for (Query q : _querySpace){//_bidStrategy.getQuerySpace()){
-				double clicksGot = qr.getClicks(q);//_unitsSold.getYesterday();
+			for (Query q : _querySpace){//_bidStrategy.getQuerySpace()){ (spucci)
+				double clicksGot = qr.getClicks(q);//_unitsSold.getYesterday(); (spucci)
 				double wantedSales = _bidStrategy.getProperty(q, JESOM2BidStrategy.WANTED_SALES);
 				double honestyFactor = _bidStrategy.getProperty(q, JESOM2BidStrategy.HONESTY_FACTOR);
 				double conversionRevenue = _bidStrategy.getProperty(q, JESOM2BidStrategy.CONVERSION_REVENUE);
 				double conversionPr = _bidStrategy.getProperty(q, JESOM2BidStrategy.CONVERSION_PR);
 
 				if (clicksGot < wantedSales/conversionPr){
-					// Didn't get enough sales, but had a good position
-					// -> raise other sales, lower this query's wanted sales
-					if (qr.getPosition(q) < 4){
+					// Didn't get enough sales, but had a good position (spucci)
+					// -> raise other sales, lower this query's wanted sales (spucci)
+					if (qr.getPosition(q) < 4){  //constant to be entered by user (spucci)
 						for (Query qu : _querySpace){
 							if (qu != q){
-								_bidStrategy.setProperty(qu, JESOM2BidStrategy.WANTED_SALES, wantedSales*1.05);
+								_bidStrategy.setProperty(qu, JESOM2BidStrategy.WANTED_SALES, wantedSales*1.05); //constant to be entered by user
 							}
 						}
-						_bidStrategy.setProperty(q, JESOM2BidStrategy.WANTED_SALES, wantedSales*.5);
+						_bidStrategy.setProperty(q, JESOM2BidStrategy.WANTED_SALES, wantedSales*.5); //constant to be entered by user
 					}
-					// Not enough sales, bad position
-					// -> raise bid
+					// Not enough sales, bad position (spucci)
+					// -> raise bid (spucci)
 					else {
-						_bidStrategy.setProperty(q, JESOM2BidStrategy.HONESTY_FACTOR, honestyFactor*1.3 + .1);
+						_bidStrategy.setProperty(q, JESOM2BidStrategy.HONESTY_FACTOR, honestyFactor*1.3 + .1); //constant to be entered by user
 						System.out.println("\n=============" + q + "\nGot: " + clicksGot + ", Wanted: " +
 								(wantedSales/conversionPr) + ", Position: " + qr.getPosition(q) + "\n=============\n");
 					}
 				}
 				else {
-					// Have enough sales, and our slot is quite low
-					// -> increase desired sales in this query, lower for others
+					// Have enough sales, and our slot is quite low (spucci)
+					// -> increase desired sales in this query, lower for others (spucci)
 					if (!(qr.getPosition(q) < 4) || qr.getCPC(q) < .2){
 						for (Query qu : _querySpace){
 							if (qu != q){
@@ -121,8 +146,8 @@ public class JESOM2Agent extends AbstractAgent {
 						}
 						_bidStrategy.setProperty(q, JESOM2BidStrategy.WANTED_SALES, wantedSales*1.6);
 					}
-					// Have enough sales, and our slot is high
-					// -> lower bid
+					// Have enough sales, and our slot is high (spucci)
+					// -> lower bid (spucci)
 					else{
 						double cpc = qr.getCPC(q)-.01;
 						double newHonesty = cpc / (conversionRevenue * conversionPr);
