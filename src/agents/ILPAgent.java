8f1588c6b61996f938e8c9d61285604acd9ea9e1
ml63 <ml63@729a0f52-b9d0-41be-9dfd-8227063685e9>
2009-05-29 16:25:31 +0000

    new ILP agent
    git-svn-id: svn://cs2955/var/svn/client/trunk@206 729a0f52-b9d0-41be-9dfd-8227063685e9

---
+++ src/agents/ILPAgent.java
@@ -0,0 +1,65 @@
+package agents;
+
+import java.util.Vector;
+
+import agents.rules.SetProperty;
+import se.sics.tasim.props.SimulationStatus;
+import edu.umich.eecs.tac.props.BidBundle;
+import edu.umich.eecs.tac.props.Product;
+
+public class ILPAgent extends AbstractAgent{
+
+	protected ILPBidStrategy _bidStrategy;
+	protected double _day;
+	Vector<Double> bids;
+	Vector<Integer> quantities;
+	
+	@Override
+	protected BidBundle buildBidBudle() {
+//		System.out.println("**********");
+//		System.out.println(_bidStrategy);
+//		System.out.println("**********");
+		return _bidStrategy.buildBidBundle();
+	}
+
+	@Override
+	protected void initBidder() {
+		// TODO Auto-generated method stub
+		ILPBidStrategy _bidStrategy = new ILPBidStrategy(_querySpace);
+		
+		_bidStrategy.setDefaultProperty(ILPBidStrategy.CAPACITY, (double)_advertiserInfo.getDistributionCapacity() / _advertiserInfo.getDistributionWindow());
+		_bidStrategy.setDefaultProperty(ILPBidStrategy.NUMUSERS, 4000); // TODO CHANGE CONSTANT
+		_bidStrategy.setDefaultProperty(ILPBidStrategy.DISCOUNTER, _advertiserInfo.getDistributionCapacityDiscounter());
+		
+		for (Product p : _retailCatalog) {
+			int givesBonus=0;
+			if (p.getManufacturer().equals(_advertiserInfo.getManufacturerSpecialty())) givesBonus = 1;
+			double profit = _retailCatalog.getSalesProfit(p)*(1+_advertiserInfo.getComponentBonus()*givesBonus);
+			_bidStrategy.setProductsRevenue(p, profit);
+		}
+		
+		bids = _bidStrategy.setPossibleBids(0, getAvaregeProductPrice(), 0.01); //TODO replace getAverageProduct with a lower bid
+		quantities = _bidStrategy.setPossibleQuantities(0, 30, 1); //TODO replace maximum quantities with a max's offer
+		System.out.println("b size = " + bids.size());
+		System.out.println("q size = " + quantities.size());
+	}
+
+	@Override
+	protected void updateBidStrategy() {
+		int numOfSlots = _slotInfo.getRegularSlots()+_slotInfo.getPromotedSlots();
+		ILPmodelsCalc ilp = new ILPmodelsCalc(bids , quantities , _retailCatalog , _queryReports , numOfSlots);
+		_bidStrategy.refreshData(ilp);		
+	}
+
+	protected void handleSimulationStatus(SimulationStatus simulationStatus) {
+		_day = simulationStatus.getCurrentDate();
+		super.handleSimulationStatus(simulationStatus);
+	}
+}
+
+/*
+ * I think there is a need for a small correction in the ILP model, we are summing over all products and queries. The problem is,
+ *  that a specific query can constrain the products that the user can look. For example, let's say that we are talking about the 
+ *  query <tv, pg>. The users that ask for this query will only buy a tv, so we don't need to sum over all products. What do you think?
+ *  Maybe a small indicator that will be equal to 1 only if the products can be purchased through this query?
+*/
