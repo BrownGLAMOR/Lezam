c2fe921c4f492ea6af72d72054d1c0c454219688
jberg <jberg@729a0f52-b9d0-41be-9dfd-8227063685e9>
2009-05-30 00:30:49 +0000

    git-svn-id: svn://cs2955/var/svn/client/trunk@209 729a0f52-b9d0-41be-9dfd-8227063685e9

---
+++ src/agents/MCKPAgentMkII.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package agents;
+
+import java.util.Set;
+
+import modelers.AbstractModel;
+import edu.umich.eecs.tac.props.BidBundle;
+import edu.umich.eecs.tac.props.QueryReport;
+import edu.umich.eecs.tac.props.SalesReport;
+
+/**
+ * @author jberg
+ *
+ */
+public class MCKPAgentMkII extends SimAbstractAgent {
+
+	/**
+	 * 
+	 */
+	public MCKPAgentMkII() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected BidBundle getBidBundle(Set<AbstractModel> models) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	protected void initBidder() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected Set<AbstractModel> updateModels(SalesReport salesReport,
+											QueryReport queryReport,
+											Set<AbstractModel> models) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
