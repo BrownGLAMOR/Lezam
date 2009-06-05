aaee927d79d53ac95d9294765fdf7b6bd539c6c9
jberg <jberg@729a0f52-b9d0-41be-9dfd-8227063685e9>
2009-06-05 16:53:42 +0000

    more perfect models
    git-svn-id: svn://cs2955/var/svn/client/trunk@245 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/simulator/models/PerfectSlotToNumClicks.java
+++ src/simulator/models/PerfectSlotToNumClicks.java
@@ -4,24 +4,28 @@ import edu.umich.eecs.tac.props.Query;
 import edu.umich.eecs.tac.props.QueryReport;
 import edu.umich.eecs.tac.props.SalesReport;
 import newmodels.slottonumclicks.AbstractSlotToNumClicks;
+import newmodels.slottonumimp.AbstractSlotToNumImp;
+import newmodels.slottoprclick.AbstractSlotToPrClick;
 
 public class PerfectSlotToNumClicks extends AbstractSlotToNumClicks {
 
-	public PerfectSlotToNumClicks(Query query) {
+	private AbstractSlotToPrClick _clickPrModel;
+	private AbstractSlotToNumImp _numImpModel;
+
+	public PerfectSlotToNumClicks(AbstractSlotToPrClick clickPrModel, AbstractSlotToNumImp numImpModel, Query query) {
 		super(query);
-		// TODO Auto-generated constructor stub
+		_clickPrModel = clickPrModel;
+		_numImpModel = numImpModel;
 	}
 
 	@Override
 	public int getPrediction(double slot) {
-		// TODO Auto-generated method stub
-		return 0;
+		return (int) (_clickPrModel.getPrediction(slot)*_numImpModel.getPrediction(slot));
 	}
 
 	@Override
 	public boolean updateModel(QueryReport queryReport, SalesReport salesReport) {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 }
