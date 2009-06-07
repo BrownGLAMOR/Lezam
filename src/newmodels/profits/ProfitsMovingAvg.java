d7c60047ba1fa5f764f27d2875fecd5f06846d8a
czhang <czhang@729a0f52-b9d0-41be-9dfd-8227063685e9>
2009-06-07 21:47:24 +0000

    git-svn-id: svn://cs2955/var/svn/client/trunk@257 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/newmodels/profits/ProfitsMovingAvg.java
+++ src/newmodels/profits/ProfitsMovingAvg.java
@@ -2,18 +2,33 @@ package newmodels.profits;
 
 import edu.umich.eecs.tac.props.Query;
 
-public class ProfitsMovingAvg extends AbstractProfitsModel {
+public class ProfitsMovingAvg {
 	
+	protected Query _query;
+	protected double _profit;
 	protected final double _alpha = .75;
+	protected double _latestSample;
+	
 	public ProfitsMovingAvg(Query query, double profit) {
-		super(query, profit);
-		// TODO Auto-generated constructor stub
+		this._query = query;
+		this._profit = profit;
 	}
-
-	@Override
-	public double update(double newSample) {
-		_profit = _alpha*newSample + (1 - _alpha)*_profit;
+	
+	public void update(double newSample) {
+		_latestSample = newSample;
+		_profit = _alpha*_latestSample + (1 - _alpha)*_profit;
+	}
+	
+	public Query getQuery() {
+		return _query;
+	}
+	
+	public double getProfit() {
 		return _profit;
+	}
+	
+	public double getLatestSample() {
+		return _latestSample;
 	}
 
 }
