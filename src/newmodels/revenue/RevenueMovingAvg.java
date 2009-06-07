f201e54292d830167a30586b3218172bb78fec27
czhang <czhang@729a0f52-b9d0-41be-9dfd-8227063685e9>
2009-06-07 22:01:17 +0000

    git-svn-id: svn://cs2955/var/svn/client/trunk@261 729a0f52-b9d0-41be-9dfd-8227063685e9

---
+++ src/newmodels/revenue/RevenueMovingAvg.java
@@ -0,0 +1,70 @@
+package newmodels.revenue;
+
+import edu.umich.eecs.tac.props.Product;
+import edu.umich.eecs.tac.props.Query;
+import edu.umich.eecs.tac.props.QueryType;
+import edu.umich.eecs.tac.props.RetailCatalog;
+import newmodels.usermodel.AbstractUserModel;
+
+public class RevenueMovingAvg {
+	protected final double _alpha = .75;
+	protected Query _query;
+	protected double _revenue;
+	protected double _latestSample;
+	
+	public RevenueMovingAvg(Query query, RetailCatalog retailCatalog) {
+		if (query.getType().equals(QueryType.FOCUS_LEVEL_TWO)) {
+			String manufacturer = query.getManufacturer();
+			String component = query.getComponent();
+			Product product = new Product(manufacturer, component);
+			this._revenue = retailCatalog.getSalesProfit(product);
+		}
+		else if (query.getType().equals(QueryType.FOCUS_LEVEL_ONE)) {
+			String tmp = query.getManufacturer();
+			if (tmp == null) {
+				String component = query.getComponent();
+				double avgRevenue = 0;
+				for (String manufacturer : retailCatalog.getManufacturers()) {
+					Product product = new Product(manufacturer, component);
+					avgRevenue +=  retailCatalog.getSalesProfit(product);
+				}
+				avgRevenue /= retailCatalog.getManufacturers().size();
+				this._revenue = avgRevenue;
+			}
+			else {
+				String manufacturer = query.getManufacturer();
+				double avgRevenue = 0;
+				for (String  component: retailCatalog.getComponents()) {
+					Product product = new Product(manufacturer, component);
+					avgRevenue +=  retailCatalog.getSalesProfit(product);
+				}
+				avgRevenue /= retailCatalog.getComponents().size();
+				this._revenue = avgRevenue;
+			}
+		}
+		else {
+			double avgRevenue = 0;
+			for (String manufacturer : retailCatalog.getManufacturers()) {
+				for (String  component: retailCatalog.getComponents()) {
+					Product product = new Product(manufacturer, component);
+					avgRevenue +=  retailCatalog.getSalesProfit(product);
+				}
+			}
+			avgRevenue /= retailCatalog.getManufacturers().size()*retailCatalog.getComponents().size();
+			this._revenue = avgRevenue;
+		}
+	}
+
+	public void update(double newSample) {
+		_latestSample = newSample;
+		_revenue = _alpha * _latestSample + (1 - _alpha) * _revenue;
+	}
+
+	public Query getQuery() {
+		return _query;
+	}
+	
+	public double getRevenue() {
+		return _revenue;
+	}
+}
