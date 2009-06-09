cbf1237cfb84c03ff4d82d79d4ffdb725d47d78b
jberg <jberg@729a0f52-b9d0-41be-9dfd-8227063685e9>
2009-06-09 06:46:23 +0000

    sim updates
    git-svn-id: svn://cs2955/var/svn/client/trunk@288 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/simulator/GUI/MainPanel.java
+++ src/simulator/GUI/MainPanel.java
@@ -1,5 +1,7 @@
 package simulator.GUI;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
@@ -21,10 +23,16 @@ import javax.swing.event.ChangeListener;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.DeviationRenderer;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.data.xy.YIntervalSeries;
+import org.jfree.data.xy.YIntervalSeriesCollection;
+import org.jfree.ui.RectangleInsets;
 
 import edu.umich.eecs.tac.props.Query;
 import edu.umich.eecs.tac.props.QueryReport;
@@ -45,7 +53,6 @@ public class MainPanel  extends JPanel {
 	private int _numSims;
 	private JComboBox _numSimsList;
 	private JPanel simsPanel;
-	private JSlider simSlider;
 	private Dimension _prefSize;
 	private LinkedList<LinkedList<Reports>> _reportsList;
 	private BasicSimulator _simulator;
@@ -65,31 +72,22 @@ public class MainPanel  extends JPanel {
 		_reportsList = reportsList;
 		
 		simsPanel = new JPanel(new FlowLayout());
-		String[] numSimsStrings = { "Average Simulation", "Specific Simulation" };
+		String[] numSimsStrings = { "Total Profits", "Daily Profits" };
 		_numSimsList = new JComboBox(numSimsStrings);
 		_numSimsList.setSelectedIndex(0);
 		_numSimsList.addActionListener(new NumSimsListener());
 		simsPanel.add(_numSimsList);
 		
-		int sliderStart = 1;
-		simSlider = new JSlider(JSlider.HORIZONTAL, sliderStart, _numSims, 1);
-		simSlider.setMajorTickSpacing(10);
-		simSlider.setMinorTickSpacing(1);
-		simSlider.setPaintTicks(true);
-		simSlider.setEnabled(false);
-		JLabel sliderLabel = new JLabel(""+sliderStart);
-		simSlider.addChangeListener(new SimSliderListener(sliderLabel));
-		simsPanel.add(simSlider);
-		simsPanel.add(sliderLabel);
-
+		JPanel rechartPanel = new JPanel(new FlowLayout());
+		JButton rechartButton = new JButton("Draw Chart");
+		rechartButton.addActionListener(new RechartButtonListener());
+		rechartPanel.add(rechartButton);
+		
 		JPanel chartPanel = new JPanel();
 		BoxLayout chartLayout = new BoxLayout(chartPanel,BoxLayout.PAGE_AXIS);
 		chartPanel.setLayout(chartLayout);
 		chartPanel.setSize(prefSize);
 		chartPanel.setPreferredSize(prefSize);
-		JButton reChartButton = new JButton("Draw Chart");
-		reChartButton.addActionListener(new ReChartButtonListener());
-		chartPanel.add(reChartButton);
 
 		lblChart = new JLabel();
 		chartPanel.add(lblChart);
@@ -100,64 +98,197 @@ public class MainPanel  extends JPanel {
 		newParsePanel.add(newParseButton);
 
 		this.add(simsPanel);
+		this.add(rechartPanel);
 		this.add(chartPanel);
 		this.add(newParsePanel);
 	}
 
-	public JFreeChart fullSimProfitsChart(LinkedList<LinkedList<Reports>> list, int simNum) {
-		XYSeriesCollection xySeriesCollection = new XYSeriesCollection();
-		LinkedList<Reports> reports = list.get(simNum);
-
-		/*
-		 * TODO
-		 * make our name display properly
-		 */
-		XYSeries series = new XYSeries("US!");
-		double totProfit = 0.0;
-		for(int day = 0; day < reports.size(); day++) {
-			Reports report = reports.get(day);
-			SalesReport salesReport = report.getSalesReport();
-			QueryReport queryReport = report.getQueryReport();
-			double totRevenue = 0.0;
-			double totCost = 0.0;
-			for(Query query : _simulator.getQuerySpace()) {
-				totRevenue += salesReport.getRevenue(query);
-				totCost += queryReport.getCost(query);
-			}
-			totProfit = totProfit + totRevenue - totCost;
-			series.add(day, totProfit);
+	public JFreeChart fullSimProfitsChart(LinkedList<LinkedList<Reports>> list) {
+		double[] minProfit = new double[list.get(0).size()];
+		double[] maxProfit = new double[list.get(0).size()];
+		double[] avgProfit = new double[list.get(0).size()];
+		
+		for(int i = 0; i < list.get(0).size(); i++) {
+			minProfit[i] = Double.MAX_VALUE;
+			maxProfit[i] = -Double.MAX_VALUE;
+			avgProfit[i] = 0.0;
 		}
-		xySeriesCollection.addSeries(series);
-		XYDataset xyDataset = xySeriesCollection;
+		
+		for(int day = 0; day < list.get(0).size(); day++) {
+			for(int sim = 0; sim < list.size(); sim++) {
+				Reports reports = list.get(sim).get(day);
+				SalesReport salesReport = reports.getSalesReport();
+				QueryReport queryReport = reports.getQueryReport();
+				double totRevenue = 0.0;
+				double totCost = 0.0;
+				for(Query query : _simulator.getQuerySpace()) {
+					totRevenue += salesReport.getRevenue(query);
+					totCost += queryReport.getCost(query);
+				}
+				double totProfit = totRevenue - totCost;
+				avgProfit[day] += totProfit;
+				if(totProfit > maxProfit[day]) {
+					maxProfit[day] = totProfit;
+				}
+				else if(totProfit < minProfit[day]) {
+					minProfit[day] = totProfit;
+				}
+			}
+		}
+		for(int day = 0; day < list.get(0).size(); day++) {
+			avgProfit[day] = avgProfit[day] / list.size();
+		}
+		YIntervalSeriesCollection yIntervalSeriesColl = new YIntervalSeriesCollection();
+
+		YIntervalSeries series = new YIntervalSeries(_agentIn);
+
+		double totAvgProfit = 0.0;
+		double totMinProfit = 0.0;
+		double totMaxProfit = 0.0;
+		for(int day = 0; day < list.get(0).size(); day++) {
+			totMinProfit = totAvgProfit + minProfit[day];
+			totMaxProfit = totAvgProfit + maxProfit[day];
+			totAvgProfit += avgProfit[day];
+			series.add(day, totAvgProfit, totMinProfit, totMaxProfit);
+		}
+		yIntervalSeriesColl.addSeries(series);
+		XYDataset xyDataset = yIntervalSeriesColl;
 		JFreeChart chart = ChartFactory.createXYLineChart
-		("Simulation " + (simNum+1),  // Title
+		("Simulation ",  // Title
 				"Day",           // X-Axis label
 				"Profit",           // Y-Axis label
 				xyDataset,          // Dataset
 				PlotOrientation.VERTICAL,
 				true,
 				true,
-				true);
+				false);
+		
+
+        chart.setBackgroundPaint(Color.white);
+        
+        // get a reference to the plot for further customisation...
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        
+        DeviationRenderer renderer = new DeviationRenderer(true, false);
+        renderer.setSeriesStroke(0, new BasicStroke(3.0f, BasicStroke.CAP_ROUND,
+                BasicStroke.JOIN_ROUND));
+        renderer.setSeriesStroke(0, new BasicStroke(3.0f));
+        renderer.setSeriesStroke(1, new BasicStroke(3.0f));
+        renderer.setSeriesFillPaint(0, new Color(0, 0, 255));
+        renderer.setSeriesFillPaint(1, new Color(255, 0, 0));
+        plot.setRenderer(renderer);
+
+        // change the auto tick unit selection to integer units only...
+        NumberAxis yAxis = (NumberAxis) plot.getRangeAxis();
+        yAxis.setAutoRangeIncludesZero(false);
+        yAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
 		return chart;
 	}
 
+	public JFreeChart dailyProfitsChart(LinkedList<LinkedList<Reports>> list) {
+		double[] minProfit = new double[list.get(0).size()];
+		double[] maxProfit = new double[list.get(0).size()];
+		double[] avgProfit = new double[list.get(0).size()];
+		
+		for(int i = 0; i < list.get(0).size(); i++) {
+			minProfit[i] = Double.MAX_VALUE;
+			maxProfit[i] = -Double.MAX_VALUE;
+			avgProfit[i] = 0.0;
+		}
+		
+		for(int day = 0; day < list.get(0).size(); day++) {
+			for(int sim = 0; sim < list.size(); sim++) {
+				Reports reports = list.get(sim).get(day);
+				SalesReport salesReport = reports.getSalesReport();
+				QueryReport queryReport = reports.getQueryReport();
+				double totRevenue = 0.0;
+				double totCost = 0.0;
+				for(Query query : _simulator.getQuerySpace()) {
+					totRevenue += salesReport.getRevenue(query);
+					totCost += queryReport.getCost(query);
+				}
+				double totProfit = totRevenue - totCost;
+				avgProfit[day] += totProfit;
+				if(totProfit > maxProfit[day]) {
+					maxProfit[day] = totProfit;
+				}
+				else if(totProfit < minProfit[day]) {
+					minProfit[day] = totProfit;
+				}
+			}
+		}
+		for(int day = 0; day < list.get(0).size(); day++) {
+			avgProfit[day] = avgProfit[day] / list.size();
+		}
+		YIntervalSeriesCollection yIntervalSeriesColl = new YIntervalSeriesCollection();
+
+		YIntervalSeries series = new YIntervalSeries(_agentIn);
+
+		for(int day = 0; day < list.get(0).size(); day++) {
+			series.add(day, avgProfit[day], minProfit[day], maxProfit[day]);
+		}
+		yIntervalSeriesColl.addSeries(series);
+		XYDataset xyDataset = yIntervalSeriesColl;
+		JFreeChart chart = ChartFactory.createXYLineChart
+		("Simulation ",  // Title
+				"Day",           // X-Axis label
+				"Profit",           // Y-Axis label
+				xyDataset,          // Dataset
+				PlotOrientation.VERTICAL,
+				true,
+				true,
+				false);
+		
+
+        chart.setBackgroundPaint(Color.white);
+        
+        // get a reference to the plot for further customisation...
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        
+        DeviationRenderer renderer = new DeviationRenderer(true, false);
+        renderer.setSeriesStroke(0, new BasicStroke(3.0f, BasicStroke.CAP_ROUND,
+                BasicStroke.JOIN_ROUND));
+        renderer.setSeriesStroke(0, new BasicStroke(3.0f));
+        renderer.setSeriesStroke(1, new BasicStroke(3.0f));
+        renderer.setSeriesFillPaint(0, new Color(0, 0, 255));
+        renderer.setSeriesFillPaint(1, new Color(255, 0, 0));
+        plot.setRenderer(renderer);
+
+        // change the auto tick unit selection to integer units only...
+        NumberAxis yAxis = (NumberAxis) plot.getRangeAxis();
+        yAxis.setAutoRangeIncludesZero(false);
+        yAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+		return chart;
+	}
+	
 	class RechooseFileButtonListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			_simulatorGUI.resetFile();
 		}
 	}
 	
-	class ReChartButtonListener implements ActionListener {
+	class RechartButtonListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 		    String simSelection = (String)_numSimsList.getSelectedItem();
 		    int sim = 0;
-		    if(simSelection == "Average Simulation") {
-		    	//TODO
+		    JFreeChart chart;
+		    if(simSelection.equals("Total Profits")) {
+		    	chart = fullSimProfitsChart(_reportsList);
 		    }
-		    else if (simSelection == "Specific Simulation") {
-		    		sim = simSlider.getValue() - 1;
+		    else if (simSelection.equals("Daily Profits")) {
+		    	chart = dailyProfitsChart(_reportsList);
 		    }
-			JFreeChart chart = fullSimProfitsChart(_reportsList,sim);
+		    else {
+		    	throw new RuntimeException("Bad Selection");
+		    }
 			BufferedImage image = chart.createBufferedImage((_prefSize.width*4)/5,(_prefSize.height*4)/5);
 			lblChart.setIcon(new ImageIcon(image));
 		}
@@ -169,31 +300,11 @@ public class MainPanel  extends JPanel {
 			JComboBox cb = (JComboBox)e.getSource();
 		    String simSelection = (String)cb.getSelectedItem();
 		    if(simSelection == "Average Simulation") {
-		    	if(simSlider.isEnabled()) {
-		    		simSlider.setEnabled(false);
-		    	}
 		    }
 		    else if (simSelection == "Specific Simulation") {
-		    	if(!simSlider.isEnabled()) {
-		    		simSlider.setEnabled(true);
-		    	}
 		    }
 		}
 	}
 	
-	class SimSliderListener implements ChangeListener {
-		
-		JLabel _sliderLabel;
-		
-		public SimSliderListener(JLabel sliderLabel) {
-			_sliderLabel = sliderLabel;
-		}
-		
-		public void stateChanged(ChangeEvent e) {
-			JSlider slider = (JSlider)e.getSource();
-			_sliderLabel.setText(""+slider.getValue());
-		}
-		
-	}
 
 }
