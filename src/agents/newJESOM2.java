e91d1659f3d2504ec8e7f92d4b6cdcd89105061f
hchen <hchen@729a0f52-b9d0-41be-9dfd-8227063685e9>
2009-06-11 20:08:23 +0000

    git-svn-id: svn://cs2955/var/svn/client/trunk@320 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/agents/newJESOM2.java
+++ src/agents/newJESOM2.java
@@ -37,47 +37,9 @@ public class newJESOM2 extends SimAbstractAgent{
 	public BidBundle getBidBundle(Set<AbstractModel> models) {
 		// TODO Auto-generated method stub
 		for(Query q: _querySpace){
-			double newHonest;
 			handleTopPosition(q);
-			//if we over-sold, lower our bid price to CPC
-		    double conversion = _conversionPrModel.get(q).getPrediction(_unitsSoldModel.getWindowSold()- _capacity);
-			if (conversion < _baseLineConversion.get(q)) {
-				newHonest = (_queryReport.getCPC(q)-1e-2)/(_revenue.get(q)*conversion);
-				if(newHonest < 0.1) _honestFactor.put(q, 0.1);
-				else _honestFactor.put(q,newHonest);
-			}
-			else{
-			
-				if(_queryReport.getClicks(q)*conversion < _wantedSales.get(q)){
-			        //if we sold less than what we expected, but we got good position, then lower our expectation 
-			        if(_queryReport.getPosition(q) < 3){
-					    _wantedSales.put(q, _wantedSales.get(q)*.625);
-			         }	
-			        else{
-			       //if we sold less than what we expected, and we got bad position
-			        	//if wanted sales does not tend to go over capacity, then higher our bid
-			        	if(_wantedSales.get(q) < (_capacity - _unitsSoldModel.getWindowSold())/magicDivisor){
-			        	    newHonest = _honestFactor.get(q)*1.3 + .1;
-			        		if(newHonest >= 0.95) _honestFactor.put(q, 0.95);
-			        		else _honestFactor.put(q, _honestFactor.get(q)*1.3+.1);
-			        	}
-			        }
-				}
-				else{
-				   //if we sold more than what we expected, and we got bad position, then increase our expectation
-					if (!(_queryReport.getPosition(q) < 3)|| _queryReport.getCPC(q)< 0.2){
-						_wantedSales.put(q,_wantedSales.get(q)*1.6);
-					}
-					else{
-					//if we sold more than what expected, and we got bad position, then lower the bid
-						newHonest = (_queryReport.getCPC(q)-0.01)/(_revenue.get(q)*conversion);
-						if(newHonest < 0.1) _honestFactor.put(q, 0.1);
-						else _honestFactor.put(q, (_queryReport.getCPC(q)-0.01)/(_revenue.get(q)*conversion));
-					
-					}
-				}
-			}
-		     
+		    adjustHonestFactor(q);
+		    adjustWantedSales(q);
 			if(q.getType() == QueryType.FOCUS_LEVEL_TWO)
 			{
 				_bidBundle.setBidAndAd(q, getQueryBid(q), new Ad(new Product(q.getManufacturer(), q.getComponent())));
@@ -103,7 +65,7 @@ public class newJESOM2 extends SimAbstractAgent{
 		
 		_honestFactor = new HashMap<Query, Double>();
 		for(Query q: _querySpace){
-			_honestFactor.put(q, 0.75);
+			_honestFactor.put(q, 0.4);
 		}
 		
 		_baseLineConversion = new HashMap<Query, Double>();
@@ -162,16 +124,72 @@ public class newJESOM2 extends SimAbstractAgent{
 		double clicks = Math.max(1,_wantedSales.get(q) / conversion);
 		return getQueryBid(q)*clicks;
 	}
-	
-	protected void handleTopPosition(Query q){
-	   if(_queryReport.getPosition(q)==1){
-		   if(_queryReport.getCPC(q) <= 0.8*getQueryBid(q)){
-			   double conversion = _conversionPrModel.get(q).getPrediction(_unitsSoldModel.getWindowSold()- _capacity);
-			   double newHonest = (_queryReport.getCPC(q) + 1e-2)/(_revenue.get(q)*conversion);
-			   if(newHonest < 0.1) _honestFactor.put(q, 0.1);
-			   else _honestFactor.put(q,newHonest);
-		   }
-	   }
+
+	protected void adjustHonestFactor(Query q)
+    {
+		double newHonest;
+		double conversion = _conversionPrModel.get(q).getPrediction(_unitsSoldModel.getWindowSold()- _capacity);
+	     //if we oversold, we lower our bid price to CPC
+		 if (conversion < _baseLineConversion.get(q)) {
+			newHonest = (_queryReport.getCPC(q)-1e-2)/(_revenue.get(q)*conversion);
+			if(newHonest < 0.1) newHonest = 0.1;
+			_honestFactor.put(q, newHonest);
+		}else{
+		  //if we sold less than what we expected, and we got bad position
+     	  //and also wanted sales does not tend to go over capacity, then higher our bid
+			    if(_queryReport.getClicks(q)*conversion < _wantedSales.get(q)){
+			    	if(!(_queryReport.getPosition(q) < 4)){
+			             if(_wantedSales.get(q) < (_capacity - _unitsSoldModel.getWindowSold())/magicDivisor){
+        	                   newHonest = _honestFactor.get(q)*1.3 + .1;
+        		               if(newHonest >= 0.95) newHonest = 0.95;
+        		               _honestFactor.put(q, newHonest);
+			              }
+			        }
+			    }else{
+			  //if we sold more than what expected, and we got bad position, then lower the bid
+			    	if(_queryReport.getClicks(q)*conversion >= _wantedSales.get(q)){
+			    		if(!(_queryReport.getPosition(q) < 4)){
+			    			newHonest = (_queryReport.getCPC(q)-0.01)/(_revenue.get(q)*conversion);
+							if(newHonest < 0.1) newHonest = 0.1;
+							_honestFactor.put(q,newHonest);
+			    		}
+			    	}
+			    }  
+		}
+		 
 	}
 	
+	protected void adjustWantedSales(Query q){
+		double conversion = _conversionPrModel.get(q).getPrediction(_unitsSoldModel.getWindowSold()- _capacity);
+		if(conversion >= _baseLineConversion.get(q)){
+		//if we sold less than what we expected, but we got good position, then lower our expectation
+		   if(_queryReport.getClicks(q)*conversion < _wantedSales.get(q)){
+			    if(!(_queryReport.getPosition(q)< 4)){
+				   _wantedSales.put(q, _wantedSales.get(q)*.625);
+			     }
+			     //if we sold more than what we expected, and we got bad position, then increase our expectation
+			     if (!(_queryReport.getPosition(q) < 4)|| _queryReport.getCPC(q)< 0.2){
+				    _wantedSales.put(q,_wantedSales.get(q)*1.6);
+			     }
+		    }else{
+		   //if we sold more than what we expected, and we got bad position, then increase our expectation
+			    if (!(_queryReport.getPosition(q) < 4)|| _queryReport.getCPC(q)< 0.2){
+				    _wantedSales.put(q,_wantedSales.get(q)*1.6);
+			    }
+		    }
+	}
+		
+	}
+	
+	protected void handleTopPosition(Query q){
+		if(_queryReport.getPosition(q)==1){
+			if(_queryReport.getCPC(q)< 0.8*getQueryBid(q)){
+				double conversion = _conversionPrModel.get(q).getPrediction(_unitsSoldModel.getWindowSold()- _capacity);
+				double newHonest = newHonest = (_queryReport.getCPC(q)+0.01)/(_revenue.get(q)*conversion);
+				if(newHonest < 0.1) newHonest = 0.1;
+				_honestFactor.put(q, newHonest);
+			}
 		}
+	}
+}
+	
