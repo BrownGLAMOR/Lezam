300451b160b08bc0ca80636c249329621d85654c
hchen <hchen@729a0f52-b9d0-41be-9dfd-8227063685e9>
2009-06-23 21:26:31 +0000

    git-svn-id: svn://cs2955/var/svn/client/trunk@422 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/agents/NewG3.java
+++ src/agents/NewG3.java
@@ -20,29 +20,54 @@ import edu.umich.eecs.tac.props.QueryType;
 import edu.umich.eecs.tac.props.SalesReport;
 
 public class NewG3 extends SimAbstractAgent{
-
+    HashMap<Query, Double> _baselineConv;
+	
 	@Override
 	public BidBundle getBidBundle(Set<AbstractModel> models) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void initBidder() {
-		// TODO Auto-generated method stub
-		
+	    _baselineConv = new HashMap<Query,Double>();
+	    for(Query query:_querySpace){
+	    	if(query.getType()== QueryType.FOCUS_LEVEL_ZERO) _baselineConv.put(query, 0.1);
+	    	if(query.getType()== QueryType.FOCUS_LEVEL_ONE){
+	    		if(query.getComponent().equals(_compSpecialty)) _baselineConv.put(query,0.27);
+	    		else _baselineConv.put(query,0.2);
+	    	}
+	    	if(query.getType()== QueryType.FOCUS_LEVEL_TWO){
+	    		if(query.getComponent().equals(_compSpecialty)) _baselineConv.put(query,0.39);
+	    		else _baselineConv.put(query,0.3);
+	    	}
+	    }
 	}
 
 	@Override
 	public Set<AbstractModel> initModels() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void updateModels(SalesReport salesReport, QueryReport queryReport) {
-		// TODO Auto-generated method stub
 		
 	}
 
+
+   public double solveK(){
+	  double dailyLimit = _capacity/5;
+	  //sum of prob(conv) across queries
+	  double sum1 = 0.0;
+	  //sum of 5*prob(conv) for queries with manufacture specialty
+	  double sum2 = 0.0;
+	  for (Query query:_querySpace){
+		  if(query.getManufacturer().equals(_manSpecialty)){
+			  sum1 += _baselineConv.get(query);
+			  sum2 += _baselineConv.get(query)*5;
+		  }
+		  else sum1 += _baselineConv.get(query);
+	  }
+	  return (dailyLimit - sum2)/sum1;
+   }
+   
 }
