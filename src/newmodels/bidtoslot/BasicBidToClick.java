34f2ee4bdd33e6ef8c6f99778838133032d06917
hchen <hchen@729a0f52-b9d0-41be-9dfd-8227063685e9>
2009-06-24 22:00:41 +0000

    git-svn-id: svn://cs2955/var/svn/client/trunk@437 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/newmodels/bidtoslot/BasicBidToClick.java
+++ src/newmodels/bidtoslot/BasicBidToClick.java
@@ -4,10 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import newmodels.bidtoslot.AbstractBidToSlotModel;
-
 import regressions.WLSRegression;
-
-
 import edu.umich.eecs.tac.props.Query;
 import edu.umich.eecs.tac.props.QueryReport;
 import edu.umich.eecs.tac.props.SalesReport;
@@ -20,11 +17,11 @@ public class BasicBidToClick {
 	private boolean _isLogLinear = false;
 	private int _weight;
 	
-	//The position function is psi*e^{zeta*bid}
-	// or psi - zeta*bid
+	//The click function is psi*e^{zeta*bid}
+	// or psi + zeta*bid
 	double PSI, ZETA;
 	
-	//PositionGivenBid is a separate calculation for each query, so we instantiate it
+	//ClickGivenBid is a separate calculation for each query, so we instantiate it
 	//with a specific query
 	public BasicBidToClick(Query q, boolean isLogLinear) {
 		_query = q;
@@ -45,11 +42,11 @@ public class BasicBidToClick {
 		return ZETA;
 	}
 
-	public void addDataPoint(int time, double bid, double position) {
+	public void addDataPoint(int time, double bid, double click) {
 		double[] arr = new double[3];
 		arr[0] = (double)time;
 		arr[1] = bid;
-		arr[2] = position;
+		arr[2] = click;
 		_dataPoints.add(arr);
 	}
 	
@@ -63,16 +60,16 @@ public class BasicBidToClick {
 	}
 
 	public boolean updateModel(SalesReport salesReport, QueryReport queryReport) {
-		//double pos = queryReport.getPosition(_query);
+		double pos = queryReport.getPosition(_query);
 		double click = queryReport.getClicks(_query);
 		double cpc = queryReport.getCPC(_query);
 		//Make sure that we actually got a position
-		if(!Double.isNaN(cpc)) {
+		if(!(Double.isNaN(pos) || Double.isNaN(cpc))) {
 			addDataPoint(_weight, cpc, click);
 		}
 		_weight++;
 		
-		if(_dataPoints.size() < 2) {
+		if(_dataPoints.size() < 1) {
 			return false;
 		}
 		//prepare matrices for regression
@@ -115,5 +112,4 @@ public class BasicBidToClick {
 
 		return false;
 	}
-
 }
