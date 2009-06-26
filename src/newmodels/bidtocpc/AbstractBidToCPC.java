50df2aff004ab613036a5f9272ad6034532d02e5
jberg <jberg@729a0f52-b9d0-41be-9dfd-8227063685e9>
2009-06-26 22:13:40 +0000

    git-svn-id: svn://cs2955/var/svn/client/trunk@458 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/newmodels/bidtocpc/AbstractBidToCPC.java
+++ src/newmodels/bidtocpc/AbstractBidToCPC.java
@@ -6,10 +6,17 @@ package newmodels.bidtocpc;
  *
  */
 
+import java.util.Set;
+
+import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.REngineException;
+import org.rosuda.REngine.Rserve.RserveException;
+
 import newmodels.AbstractModel;
 import edu.umich.eecs.tac.props.Query;
 import edu.umich.eecs.tac.props.QueryReport;
 import edu.umich.eecs.tac.props.SalesReport;
+import edu.umich.eecs.tac.props.BidBundle;
 
 /**
  * @author jberg
@@ -17,19 +24,19 @@ import edu.umich.eecs.tac.props.SalesReport;
  */
 public abstract class AbstractBidToCPC extends AbstractModel {
 	
-	protected Query _query;
-
-	public AbstractBidToCPC(Query query) {
-		_query = query;
+	public AbstractBidToCPC() {
 	}
 	
+	/**
 	public abstract boolean updateModel(QueryReport queryReport,
 									SalesReport salesReport);
+	 * @throws REXPMismatchException 
+	 * @throws RserveException 
+	 * @throws REngineException 
+	*/
 	
-	public abstract double getPrediction(double bid);
+	public abstract double getPrediction(Query query, double bid, BidBundle bidbundle) throws RserveException, REXPMismatchException, REngineException;
 
-	public Query getQuery() {
-		return _query;
-	}
+	public abstract boolean updateModel(QueryReport queryreport, BidBundle bidbundle) throws RserveException, REngineException, REXPMismatchException;
 	
 }
