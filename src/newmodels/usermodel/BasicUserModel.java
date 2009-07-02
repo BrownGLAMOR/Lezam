316309f8312fcb195d447e2059cf13efadb3bb82
jberg <jberg@729a0f52-b9d0-41be-9dfd-8227063685e9>
2009-07-02 15:03:42 +0000

    git-svn-id: svn://cs2955/var/svn/client/trunk@512 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/newmodels/usermodel/BasicUserModel.java
+++ src/newmodels/usermodel/BasicUserModel.java
@@ -14,46 +14,24 @@ import edu.umich.eecs.tac.props.SalesReport;
 
 public class BasicUserModel extends AbstractUserModel {
 	
-	private Random _R = new Random();					//Random number generator
-	private double minF0 = 1755;
-	private double maxF0 = 8716;
-	private double minF1 = 1734;
-	private double maxF1 = 5060;
-	private double minF2 = 1412;
-	private double maxF2 = 4699;
-	private double minIS = 895;
-	private double maxIS = 9111;
-	private double minT = 1729;
-	private double maxT = 822;
-	private int numUsers = 90000;
 	private double F0users;
 	private double F2users;
 	private double F1users;
 	private double ISusers;
 	private double Tusers;
 	private double NSusers;
-	private int numProducts = 9;
 
 	public BasicUserModel() {
-		F0users = randGaussian(minF0,maxF0)/numProducts;
-		F1users = randGaussian(minF1,maxF1)/numProducts;
-		F2users = randGaussian(minF2,maxF2)/numProducts;
-		ISusers = randGaussian(minIS,maxIS)/numProducts;
-		Tusers = randGaussian(minT,maxT)/numProducts;
-		NSusers = (numUsers/numProducts - F0users - F1users - F2users - ISusers - Tusers);
+		F0users = 570;
+		F1users = 412;
+		F2users = 444;
+		ISusers = 301;
+		Tusers = 18;
+		NSusers = 8255;
 	}
 	
 	@Override
 	public int getPrediction(Product product, UserState userState) {
-		
-		/*
-		 * I just simulated a game for a really long time to get the minimum
-		 * and maximum % of users in F0,F1, and F2..Then I basically just
-		 * pick a normally distributed number based on the min and max.
-		 * 
-		 * This function returns an array of the number of users in a given
-		 * query class.  Index 0 is F0, 1 is F1, and 2 is F2
-		 */
 
 		if(userState == UserState.F0) {
 			return (int) F0users;
@@ -83,12 +61,4 @@ public class BasicUserModel extends AbstractUserModel {
 		return true;
 	}
 	
-	private double randGaussian(double a, double b) {
-		double rand = _R.nextGaussian();
-		double mean = (a+b)/2;
-		//How many deviations away from the mean is the min value
-		double deviations = 3;
-		double stddev = (mean-a)/deviations;
-		return rand*stddev+mean;
-	}
 }
