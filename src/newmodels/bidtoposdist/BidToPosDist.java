1cd77a19d71b3e723286ce204bcb803296d155a0
jberg <jberg@729a0f52-b9d0-41be-9dfd-8227063685e9>
2009-08-22 21:46:13 +0000

    bid->dist(pos)
    git-svn-id: svn://cs2955/var/svn/client/trunk@879 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/newmodels/bidtoposdist/BidToPosDist.java
+++ src/newmodels/bidtoposdist/BidToPosDist.java
@@ -24,58 +24,58 @@ public class BidToPosDist extends AbstractBidToPosDistModel {
 	private Set<Query> _querySpace;
 	private HashMap<Query, ArrayList<Double>> _bids;
 	private HashMap<Query, ArrayList<double[]>> _posDists;
-	private HashMap<Query,Integer> _queryToInt;
+	private HashMap<Query, double[]> _coefficients;
 	private ArrayList<QueryReport> _queryReports;
 	private ArrayList<BidBundle> _bidBundles;
-	private HashMap<Query, REXP> _surfaces;
+	private int _degree;
 
-	public BidToPosDist(RConnection rConnection, Set<Query> querySpace) {
+	public BidToPosDist(RConnection rConnection, Set<Query> querySpace, int degree) {
 		_rConnection = rConnection;
-		try {
-			_rConnection.voidEval("library(spatial)");
-		} catch (RserveException e) {
-			throw new RuntimeException("Could not load the R spatial library");
-		}
 		_querySpace = querySpace;
-		_surfaces = new HashMap<Query,REXP>();
 		_bids = new HashMap<Query,ArrayList<Double>>();
 		_posDists = new HashMap<Query,ArrayList<double[]>>();
+		_coefficients = new HashMap<Query, double[]>();
 		_queryReports = new ArrayList<QueryReport>();
 		_bidBundles = new ArrayList<BidBundle>();
-		_queryToInt = new HashMap<Query, Integer>();
-		int i = 0;
 		for(Query query : _querySpace) {
 			ArrayList<Double> bids = new ArrayList<Double>();
 			ArrayList<double[]> posDists = new ArrayList<double[]>();
 			_bids.put(query, bids);
 			_posDists.put(query, posDists);
-			_surfaces.put(query, null);
-			_queryToInt.put(query, i);
-			i++;
+			_coefficients.put(query, null);
 		}
+		_degree = degree;
 	}
 
 	@Override
 	public double[] getPrediction(Query query, double bid) {
+		double[] coeff = _coefficients.get(query);
+		if(coeff == null) {
+			return null;
+		}
 		double predictions[] = new double[5];
 		for(int i = 0; i < predictions.length; i++) {
-			predictions[i] = 0.0;
-		}
-		try {
-			for(int i = 0; i < predictions.length; i++) {
-				double prediction = _rConnection.eval("predict.trls(surf" + _queryToInt.get(query) + "," + bid + "," + (i+1) + ")").asDouble();
-				//				System.out.println("Pred: " + prediction);
-				predictions[i] = prediction;
+			int posDist = i+1;
+			double prediction = 0.0;
+			if(_degree == 1) {
+				prediction = coeff[0] + coeff[1] * posDist + coeff[2] * bid + coeff[3] * bid * posDist;
 			}
-			return predictions;
-		} catch (RserveException e) {
-			System.out.println(_rConnection.getLastError());
-			e.printStackTrace();
-			return null;
-		} catch (REXPMismatchException e) {
-			e.printStackTrace();
-			return null;
+			else if(_degree == 2) {
+				prediction = coeff[0] + coeff[1] * posDist + coeff[2] * posDist * posDist + coeff[3] * bid + coeff[4] * bid * posDist + coeff[5] * bid * posDist * posDist + coeff[6] * bid * bid + coeff[7] * bid * bid * posDist + coeff[8] * bid * bid * posDist * posDist;
+			}
+			else if(_degree == 3) {
+				prediction = coeff[0] + coeff[1] * posDist + coeff[2] * posDist * posDist + coeff[3] * posDist * posDist * posDist + coeff[4] * bid + coeff[5] * bid * posDist + coeff[6] * bid * posDist * posDist + coeff[7] * bid * posDist * posDist * posDist + coeff[8] * bid * bid + coeff[9] * bid * bid * posDist + coeff[10] * bid * bid * posDist * posDist + coeff[11] * bid * bid * posDist * posDist * posDist + coeff[12] * bid * bid * bid + coeff[13] * bid * bid * bid * posDist + coeff[14] * bid * bid * bid * posDist * posDist + coeff[15] * bid * bid * bid * posDist * posDist * posDist;
+			}
+			else if(_degree == 4) {
+				prediction = coeff[0] + coeff[1] * posDist + coeff[2] * posDist * posDist + coeff[3] * posDist * posDist * posDist + coeff[4] * posDist * posDist * posDist * posDist + coeff[5] * bid + coeff[6] * bid * posDist + coeff[7] * bid * posDist * posDist + coeff[8] * bid * posDist * posDist * posDist + coeff[9] * bid * posDist * posDist * posDist * posDist + coeff[10] * bid * bid + coeff[11] * bid * bid * posDist + coeff[12] * bid * bid * posDist * posDist + coeff[13] * bid * bid * posDist * posDist * posDist + coeff[14] * bid * bid * posDist * posDist * posDist * posDist + coeff[15] * bid * bid * bid + coeff[16] * bid * bid * bid * posDist + coeff[17] * bid * bid * bid * posDist * posDist + coeff[18] * bid * bid * bid * posDist * posDist * posDist + coeff[19] * bid * bid * bid * posDist * posDist * posDist * posDist + coeff[20] * bid * bid * bid * bid + coeff[21] * bid * bid * bid * bid * posDist + coeff[22] * bid * bid * bid * bid * posDist * posDist + coeff[23] * bid * bid * bid * bid * posDist * posDist * posDist + coeff[24] * bid * bid * bid * bid * posDist * posDist * posDist * posDist;
+			}
+			else if(_degree == 5) {
+				prediction = coeff[0] + coeff[1] * posDist + coeff[2] * posDist * posDist + coeff[3] * posDist * posDist * posDist + coeff[4] * posDist * posDist * posDist * posDist + coeff[5] * posDist * posDist * posDist * posDist * posDist + coeff[6] * bid + coeff[7] * bid * posDist + coeff[8] * bid * posDist * posDist + coeff[9] * bid * posDist * posDist * posDist + coeff[10] * bid * posDist * posDist * posDist * posDist + coeff[11] * bid * posDist * posDist * posDist * posDist * posDist + coeff[12] * bid * bid + coeff[13] * bid * bid * posDist + coeff[14] * bid * bid * posDist * posDist + coeff[15] * bid * bid * posDist * posDist * posDist + coeff[16] * bid * bid * posDist * posDist * posDist * posDist + coeff[17] * bid * bid * posDist * posDist * posDist * posDist * posDist + coeff[18] * bid * bid * bid + coeff[19] * bid * bid * bid * posDist + coeff[20] * bid * bid * bid * posDist * posDist + coeff[21] * bid * bid * bid * posDist * posDist * posDist + coeff[22] * bid * bid * bid * posDist * posDist * posDist * posDist + coeff[23] * bid * bid * bid * posDist * posDist * posDist * posDist * posDist + coeff[24] * bid * bid * bid * bid + coeff[25] * bid * bid * bid * bid * posDist + coeff[26] * bid * bid * bid * bid * posDist * posDist + coeff[27] * bid * bid * bid * bid * posDist * posDist * posDist + coeff[28] * bid * bid * bid * bid * posDist * posDist * posDist * posDist + coeff[29] * bid * bid * bid * bid * posDist * posDist * posDist * posDist * posDist + coeff[30] * bid * bid * bid * bid * bid + coeff[31] * bid * bid * bid * bid * bid * posDist + coeff[32] * bid * bid * bid * bid * bid * posDist * posDist + coeff[33] * bid * bid * bid * bid * bid * posDist * posDist * posDist + coeff[34] * bid * bid * bid * bid * bid * posDist * posDist * posDist * posDist + coeff[35] * bid * bid * bid * bid * bid * posDist * posDist * posDist * posDist * posDist;
+			}
+			//				System.out.println("Pred: " + prediction);
+			predictions[i] = prediction;
 		}
+		return predictions;
 	}
 
 	private double[] normalizeArr(double[] predictions) {
@@ -120,6 +120,9 @@ public class BidToPosDist extends AbstractBidToPosDistModel {
 
 			bids.add(bid);
 			pDists.add(posDist);
+
+			_bids.put(query, bids);
+			_posDists.put(query, pDists);
 		}
 
 		if(_bids.get(new Query(null,null)).size() > 3) {
@@ -144,9 +147,32 @@ public class BidToPosDist extends AbstractBidToPosDistModel {
 					_rConnection.assign("bids", bids);
 					_rConnection.assign("pos", pos);
 					_rConnection.assign("posDist", posDist);
-					_rConnection.eval("surf" + _queryToInt.get(query) +  " = "  + "surf.ls(3,bids,pos,posDist" + ")");
+					String model = "lm(";
+					if(_degree == 1) {
+						model += "pos ~ posDist + bid + bid * posDist";
+					}
+					else if(_degree == 2) {
+						model += "pos ~ posDist + posDist^2 + bid + bid * posDist + bid * posDist^2 + bid^2 + bid^2 * posDist + bid^2 * posDist^2";
+					}
+					else if(_degree == 3) {
+						model += "pos ~ posDist + posDist^2 + posDist^3 + bid + bid * posDist + bid * posDist^2 + bid * posDist^3 + bid^2 + bid^2 * posDist + bid^2 * posDist^2 + bid^2 * posDist^3 + bid^3 + bid^3 * posDist + bid^3 * posDist^2 + bid^3 * posDist^3";
+					}
+					else if(_degree == 4) {
+						model += "pos ~ posDist + posDist^2 + posDist^3 + posDist^4 + bid + bid * posDist + bid * posDist^2 + bid * posDist^3 + bid * posDist^4 + bid^2 + bid^2 * posDist + bid^2 * posDist^2 + bid^2 * posDist^3 + bid^2 * posDist^4 + bid^3 + bid^3 * posDist + bid^3 * posDist^2 + bid^3 * posDist^3 + bid^3 * posDist^4 + bid^4 + bid^4 * posDist + bid^4 * posDist^2 + bid^4 * posDist^3 + bid^4 * posDist^4";
+					}
+					else if(_degree == 5) {
+						model += "pos ~ posDist + posDist^2 + posDist^3 + posDist^4 + posDist^5 + bid + bid * posDist + bid * posDist^2 + bid * posDist^3 + bid * posDist^4 + bid * posDist^5 + bid^2 + bid^2 * posDist + bid^2 * posDist^2 + bid^2 * posDist^3 + bid^2 * posDist^4 + bid^2 * posDist^5 + bid^3 + bid^3 * posDist + bid^3 * posDist^2 + bid^3 * posDist^3 + bid^3 * posDist^4 + bid^3 * posDist^5 + bid^4 + bid^4 * posDist + bid^4 * posDist^2 + bid^4 * posDist^3 + bid^4 * posDist^4 + bid^4 * posDist^5 + bid^5 + bid^5 * posDist + bid^5 * posDist^2 + bid^5 * posDist^3 + bid^5 * posDist^4 + bid^5 * posDist^5";
+					}
+					
+					model += ")";
+					_rConnection.voidEval(model);
+					double[] coeff = _rConnection.eval("coefficients(model)").asDoubles();
+					_coefficients.put(query, coeff);
 				} catch (REngineException e) {
-					System.out.println(_rConnection.getLastError());
+					_coefficients.put(query, null);
+					e.printStackTrace();
+				} catch (REXPMismatchException e) {
+					_coefficients.put(query, null);
 					e.printStackTrace();
 				}
 			}
@@ -159,8 +185,75 @@ public class BidToPosDist extends AbstractBidToPosDistModel {
 
 	@Override
 	public AbstractModel getCopy() {
-		return new BidToPosDist(_rConnection, _querySpace);
+		return new BidToPosDist(_rConnection, _querySpace, _degree);
 	}
-
-
 }
+
+
+/*
+ * 
+ * 
+ * I used the following two python scripts to generate the different surface strings, will put in java later:
+ * 
+ * 
+ *
+degree = 5
+model = 'pos ~ '
+for x in range(0,degree+1):
+	for y in range(0,degree+1):
+		if(x > 0):
+			if(x == 1):
+				model += "bid"
+			if(x > 1):
+				model += "bid^" + str(x)
+			if(y > 0):
+				model += " * "
+		if(y > 0):
+			if(y == 1):
+				model += "posDist"
+			if(y > 1):
+				model += "posDist^" + str(y)
+		if(x > 0 or y > 0):
+			model += " + "
+
+model = model[:-3]
+model
+ * 
+ *
+ * and
+ * 
+ * 
+ * 
+degree = 5
+model = 'prediction = '
+coeff = 0;
+for x in range(0,degree+1):
+	for y in range(0,degree+1):
+		if(x == 0 and y == 0):
+			model += "coeff[" + str(coeff) + "] + "
+			coeff += 1
+		if(x > 0):
+			model += "coeff[" + str(coeff) + "] * "
+			coeff += 1
+			if(x == 1):
+				model += "bid"
+			if(x > 1):
+				model += "bid * " * x
+				model = model[:-3]
+			if(y > 0):
+				model += " * "
+		if(y > 0):
+			if(x == 0):
+				model += "coeff[" + str(coeff) + "] * "
+				coeff += 1
+			if(y == 1):
+				model += "posDist"
+			if(y > 1):
+				model += "posDist * " * y
+				model = model[:-3]
+		if(x > 0 or y > 0):
+			model += " + "
+
+model = model[:-3] + ";"
+model
+*/
