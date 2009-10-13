6ebef96bbb3bcebb1ffd3fa902a54cbd6b1d8ca0
jberg <jberg@729a0f52-b9d0-41be-9dfd-8227063685e9>
2009-10-13 06:04:59 +0000

    git-svn-id: svn://cs2955/var/svn/client/trunk@1267 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/agents/MCKPPos.java
+++ src/agents/MCKPPos.java
@@ -339,7 +339,7 @@ public class MCKPPos extends AbstractAgent {
 		}
 
 
-		if(_day > lagDays || models != null){
+		if(_day > lagDays){
 			buildMaps(models);
 			//NEED TO USE THE MODELS WE ARE PASSED!!!
 
@@ -494,15 +494,15 @@ public class MCKPPos extends AbstractAgent {
 					/*
 					 * We decided that we did not want to be in this query, so we will use it to explore the space
 					 */
-					bid = 0.0;
-					bidBundle.addQuery(q, bid, new Ad(), Double.NaN);
+					//					bid = 0.0;
+					//					bidBundle.addQuery(q, bid, new Ad(), Double.NaN);
 					//					System.out.println("Bidding " + bid + "   for query: " + q);
-					//					if (q.getType().equals(QueryType.FOCUS_LEVEL_ZERO))
-					//						bid = randDouble(.04,_salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .9);
-					//					else if (q.getType().equals(QueryType.FOCUS_LEVEL_ONE))
-					//						bid = randDouble(.04,_salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .9);
-					//					else
-					//						bid = randDouble(.04,_salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .9);
+					if (q.getType().equals(QueryType.FOCUS_LEVEL_ZERO))
+						bid = randDouble(.04,_salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .9);
+					else if (q.getType().equals(QueryType.FOCUS_LEVEL_ONE))
+						bid = randDouble(.04,_salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .9);
+					else
+						bid = randDouble(.04,_salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .9);
 
 					//					System.out.println("Exploring " + q + "   bid: " + bid);
 					bidBundle.addQuery(q, bid, new Ad(), bid*10);
@@ -613,7 +613,7 @@ public class MCKPPos extends AbstractAgent {
 				}
 				double valueLostWindow = Math.max(1, Math.min(_capWindow, 59 - _day));
 				valueLostWindow *= 1.25;
-				
+
 				for (int i = _capacityInc*knapSackIter+1; i <= _capacityInc*(knapSackIter+1); i++){
 					double iD = Math.pow(LAMBDA, i);
 					double worseConvProb = avgConvProb*iD; //this is a gross average that lacks detail
