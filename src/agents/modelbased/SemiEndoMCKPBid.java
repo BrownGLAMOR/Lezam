4459f9e5078775d1762e6ea077d9d39d2c7779de
jberg <jberg@729a0f52-b9d0-41be-9dfd-8227063685e9>
2010-02-22 19:20:13 +0000

    git-svn-id: svn://cs2955/var/svn/client/trunk@1633 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/agents/modelbased/SemiEndoMCKPBid.java
+++ src/agents/modelbased/SemiEndoMCKPBid.java
@@ -82,12 +82,12 @@ public class SemiEndoMCKPBid extends AbstractAgent {
 		BUDGET = budget;
 		FORWARDUPDATING = forward;
 		PRICELINES = pricelines;
-		_R.setSeed(124962748);
+//		_R.setSeed(124962748);
 		bidList = new ArrayList<Double>();
 		//		double increment = .25;
 		double increment  = .05;
-		double min = .04;
-		double max = 1.65;
+		double min = .55;
+		double max = 2.0;
 		int tot = (int) Math.ceil((max-min) / increment);
 		for(int i = 0; i < tot; i++) {
 			bidList.add(min+(i*increment));
@@ -398,11 +398,11 @@ public class SemiEndoMCKPBid extends AbstractAgent {
 					//					System.out.println("Bidding " + bid + "   for query: " + q);
 
 					if (q.getType().equals(QueryType.FOCUS_LEVEL_ZERO))
-						bid = randDouble(.04,_salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .7);
+						bid = randDouble(.4,_salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .8);
 					else if (q.getType().equals(QueryType.FOCUS_LEVEL_ONE))
-						bid = randDouble(.04,_salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .7);
+						bid = randDouble(.4,_salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .8);
 					else
-						bid = randDouble(.04,_salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .7);
+						bid = randDouble(.4,_salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .8);
 
 					//					System.out.println("Exploring " + q + "   bid: " + bid);
 					bidBundle.addQuery(q, bid, new Ad(), bid*5);
@@ -418,11 +418,11 @@ public class SemiEndoMCKPBid extends AbstractAgent {
 		else {
 			for(Query q : _querySpace){
 				if(_compSpecialty.equals(q.getComponent()) || _manSpecialty.equals(q.getManufacturer())) {
-					double bid = randDouble(_salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .35, _salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .65);
+					double bid = randDouble(_salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .5, _salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .8);
 					bidBundle.addQuery(q, bid, new Ad(), Double.MAX_VALUE);
 				}
 				else {
-					double bid = randDouble(.04,_salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .65);
+					double bid = randDouble(.4,_salesPrices.get(q) * _baseConvProbs.get(q) * _baseClickProbs.get(q) * .8);
 					bidBundle.addQuery(q, bid, new Ad(), bid*10);
 				}
 			}
@@ -431,6 +431,11 @@ public class SemiEndoMCKPBid extends AbstractAgent {
 		double stop = System.currentTimeMillis();
 		double elapsed = stop - start;
 		//		System.out.println("This took " + (elapsed / 1000) + " seconds");
+		for(Query q : _querySpace) {
+			if(Double.isNaN(bidBundle.getBid(q)) || bidBundle.getBid(q) < 0) {
+				bidBundle.setBid(q, 0.0);
+			}
+		}
 		return bidBundle;
 	}
 
