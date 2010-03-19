8101d17f4ae7a8b0c48dfd156b68ffdf14368d1f
jberg <jberg@729a0f52-b9d0-41be-9dfd-8227063685e9>
2010-03-19 05:00:17 +0000

    git-svn-id: svn://cs2955/var/svn/client/trunk@1676 729a0f52-b9d0-41be-9dfd-8227063685e9

---
+++ src/models/usermodel/OldUserModel.java
@@ -0,0 +1,76 @@
+package models.usermodel;
+
+/**
+ * @author jberg
+ *
+ */
+
+import java.util.HashMap;
+import java.util.Random;
+
+import models.AbstractModel;
+import models.usermodel.TacTexAbstractUserModel.UserState;
+import edu.umich.eecs.tac.props.Product;
+import edu.umich.eecs.tac.props.Query;
+import edu.umich.eecs.tac.props.QueryReport;
+import edu.umich.eecs.tac.props.SalesReport;
+
+public class OldUserModel extends TacTexAbstractUserModel {
+	
+	private double F0users;
+	private double F2users;
+	private double F1users;
+	private double ISusers;
+	private double Tusers;
+	private double NSusers;
+
+	public OldUserModel() {
+		F0users = 570;
+		F1users = 412;
+		F2users = 444;
+		ISusers = 301;
+		Tusers = 18;
+		NSusers = 8255;
+	}
+	
+	@Override
+	public AbstractModel getCopy() {
+		return new OldUserModel();
+	}
+
+	@Override
+	public int getCurrentEstimate(Product product, UserState userState) {
+		if(userState == UserState.F0) {
+			return (int) F0users;
+		}
+		else if(userState == UserState.F1) {
+			return (int) F1users;
+		}
+		else if(userState == UserState.F2) {
+			return (int) F2users;
+		}
+		else if(userState == UserState.IS) {
+			return (int) ISusers;
+		}
+		else if(userState == UserState.T) {
+			return (int) Tusers;
+		}
+		else if(userState == UserState.NS) {
+			return (int) NSusers;
+		}
+		else {
+			throw new RuntimeException("");
+		}
+	}
+
+	@Override
+	public int getPrediction(Product product, UserState userState) {
+		return getCurrentEstimate(product, userState);
+	}
+
+	@Override
+	public boolean updateModel(HashMap<Query, Integer> totalImpressions) {
+		return true;
+	}
+	
+}
