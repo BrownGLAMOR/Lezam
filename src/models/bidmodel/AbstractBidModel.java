bca4378344d10c1e35bb9d9221e94954ac0bb491
estix <estix@729a0f52-b9d0-41be-9dfd-8227063685e9>
2010-05-12 00:43:55 +0000

    made max reasonable bid protected
    git-svn-id: svn://cs2955/var/svn/client/trunk@1849 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/models/bidmodel/AbstractBidModel.java
+++ src/models/bidmodel/AbstractBidModel.java
@@ -9,7 +9,7 @@ import edu.umich.eecs.tac.props.QueryReport;
 import edu.umich.eecs.tac.props.SalesReport;
 
 public abstract class AbstractBidModel extends AbstractModel {
-	private static final double maxReasonableBid = 3.75;//TODO: CHANGE THIS TO THE APPROPRIATE NEW VALUE
+	protected static final double maxReasonableBid = 3.75;//TODO: CHANGE THIS TO THE APPROPRIATE NEW VALUE
 	
 	public abstract boolean updateModel(double cpc, double ourBid, HashMap<String, Integer> ranks);
 	public abstract double getPrediction(String player);
