5c920a4110f4ecf052ff4399304a8b78a8efe921
estix <estix@729a0f52-b9d0-41be-9dfd-8227063685e9>
2010-05-12 01:22:28 +0000

    git-svn-id: svn://cs2955/var/svn/client/trunk@1850 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/models/bidmodel/AbstractBidModel.java
+++ src/models/bidmodel/AbstractBidModel.java
@@ -5,12 +5,13 @@ import java.util.HashMap;
 import models.AbstractModel;
 import models.usermodel.TacTexAbstractUserModel.UserState;
 import edu.umich.eecs.tac.props.Product;
+import edu.umich.eecs.tac.props.Query;
 import edu.umich.eecs.tac.props.QueryReport;
 import edu.umich.eecs.tac.props.SalesReport;
 
 public abstract class AbstractBidModel extends AbstractModel {
 	protected static final double maxReasonableBid = 3.75;//TODO: CHANGE THIS TO THE APPROPRIATE NEW VALUE
 	
-	public abstract boolean updateModel(double cpc, double ourBid, HashMap<String, Integer> ranks);
-	public abstract double getPrediction(String player);
+	public abstract boolean updateModel(double cpc, HashMap<Query, Double> ourBid, HashMap<Query, HashMap<String, Integer>> ranks);
+	public abstract double getPrediction(String player, Query q);
 }
