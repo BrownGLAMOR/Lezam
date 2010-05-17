c5c28f8ff2da92e374a7e1d6d93a62105b917d1e
jberg <jberg@729a0f52-b9d0-41be-9dfd-8227063685e9>
2010-05-17 22:55:43 +0000

    better maxbids
    git-svn-id: svn://cs2955/var/svn/client/trunk@1897 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/models/bidmodel/AbstractBidModel.java
+++ src/models/bidmodel/AbstractBidModel.java
@@ -10,7 +10,9 @@ import edu.umich.eecs.tac.props.QueryReport;
 import edu.umich.eecs.tac.props.SalesReport;
 
 public abstract class AbstractBidModel extends AbstractModel {
-	protected static final double maxReasonableBid = 3.75;//TODO: CHANGE THIS TO THE APPROPRIATE NEW VALUE
+	protected static final double maxReasonableBidF0 = 3.5;//TODO: CHANGE THIS TO THE APPROPRIATE NEW VALUE
+	protected static final double maxReasonableBidF1 = 2.5;
+	protected static final double maxReasonableBidF2 = 1.5;
 	
 	public abstract boolean updateModel(HashMap<Query, Double> cpc, HashMap<Query, Double> ourBid, HashMap<Query, HashMap<String, Integer>> ranks);
 	public abstract double getPrediction(String player, Query q);
