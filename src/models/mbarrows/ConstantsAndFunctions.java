97387e184691bb00d63051de36fc6f460b1f8161
mbarrows <mbarrows@729a0f52-b9d0-41be-9dfd-8227063685e9>
2010-05-20 06:09:00 +0000

    git-svn-id: svn://cs2955/var/svn/client/trunk@1957 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/models/mbarrows/ConstantsAndFunctions.java
+++ src/models/mbarrows/ConstantsAndFunctions.java
@@ -45,8 +45,8 @@ public abstract class ConstantsAndFunctions
 	
 	final double[] _continuationProbBoundsAvg = {0.35,0.45,0.55};
 	
-	final double[][] fTargetfPro = { {(1.0 + _TE), (1.0 + _TE)*(1.0+_PSB)},
-	         {(1.0), (1.0)*(1.0+_PSB)},
+	final double[][] fTargetfPro = { {(1.0), (1.0)*(1.0+_PSB)},
+	         {(1.0 + _TE), (1.0 + _TE)*(1.0+_PSB)},
 	         {(1.0)/(1.0 + _TE), (1.0)/(1.0 + _TE)*(1.0+_PSB)}
 		 };
 	
