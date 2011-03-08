d45afdb969c928301e338b7f9c47885d298d6124
jberg <jberg@729a0f52-b9d0-41be-9dfd-8227063685e9>
2011-03-08 21:44:37 +0000

    fixed the discrepancy calculation in smart search
    git-svn-id: svn://cs2955/var/svn/client/trunk@2150 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/models/queryanalyzer/search/LDSSwap.java
+++ src/models/queryanalyzer/search/LDSSwap.java
@@ -1,19 +1,27 @@
 package models.queryanalyzer.search;
 
 public class LDSSwap implements Comparable<LDSSwap> {
-	int _a1;
-	int _a2;
-	
-	public LDSSwap(int a1, int a2){
-		_a1 = a1;
-		_a2 = a2;
-	}
+   int _a1;
+   int _a2;
 
-	public int compareTo(LDSSwap o) {
-		if((_a1 == o._a1 && _a2 == o._a2)){
-			return 0;
-		}
-		return 1;
-	}
-	
+   public LDSSwap(int a1, int a2) {
+      _a1 = a1;
+      _a2 = a2;
+   }
+
+   public int getFirstIdx() {
+      return _a1;
+   }
+
+   public int getSecondIdx() {
+      return _a2;
+   }
+
+   public int compareTo(LDSSwap o) {
+      if ((_a1 == o._a1 && _a2 == o._a2)) {
+         return 0;
+      }
+      return 1;
+   }
+
 }
--- src/models/queryanalyzer/search/LDSearchSmart.java
+++ src/models/queryanalyzer/search/LDSearchSmart.java
@@ -1,88 +1,105 @@
 package models.queryanalyzer.search;
 
-import java.util.Arrays;
-import java.util.HashSet;
+import models.queryanalyzer.iep.IEResult;
+
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 
-import models.queryanalyzer.iep.IEResult;
-
 abstract class LDSearchSmart {
-	private int _distFactor;
-	private int _iterations;
-	private PriorityQueue<LDSPerm> _LDSQueue;
-	
-	private int _slots;
-	IEResult _best;
-	
-	public LDSearchSmart(int slots){
-		_LDSQueue = new PriorityQueue<LDSPerm>();
-		_iterations = 0;
-		_distFactor = 10000;
-		_slots = slots;
-	}
-	
-	public void search(int[] startPerm, double[] avgPos){
-		_best = null;
-		_iterations = 0;
-		int Indexs = startPerm.length;
-		_LDSQueue.clear();
-		_LDSQueue.add(new LDSPerm(0, startPerm, new LinkedList<LDSSwap>()));
+   private int _distFactor;
+   private int _iterations;
+   private PriorityQueue<LDSPerm> _LDSQueue;
 
-		while(!_LDSQueue.isEmpty()){
-			_iterations += 1;
-			LDSPerm perm = _LDSQueue.poll();
-			int dsVal = perm.getVal();
-			
-			if(evalPerm(perm._perm)){
-				return;
-			}
+   private int _slots;
+   IEResult _best;
 
-			for(int i1=0; i1 < Indexs; i1++){
-				for(int i2=i1+1; i2 < Indexs; i2++){
-					LDSSwap nextSwap = new LDSSwap(i1,i2);
-					if(!perm._swapped.contains(nextSwap)){
-						int[] nextPerm = new int[Indexs]; 
-						for(int j=0; j < Indexs; j++){
-							nextPerm[j] = perm._perm[j];
-						}
-						
-						LinkedList<LDSSwap> nextSwapSet = new LinkedList<LDSSwap>();
-						nextSwapSet.addAll(perm._swapped);
-						nextSwapSet.add(new LDSSwap(i1,i2));
-						
-						int temp = nextPerm[i1];
-						nextPerm[i1] = nextPerm[i2];
-						nextPerm[i2] = temp;
-						
-						if(feasibleOrder(nextPerm, avgPos)){
-							int delta = (int)(100*Math.abs(avgPos[nextPerm[i1]] - avgPos[nextPerm[i2]])); //needs a little more work, becouse avp array changes with swaps
-							//System.out.println("Pushing: "+Arrays.toString(nextPerm)+" "+nextSwapSet);
-							_LDSQueue.add(new LDSPerm(dsVal+_distFactor+delta, nextPerm, nextSwapSet));
-						}
-					}
-				}
-			}
-		}
-	}
-	
-	public int getIterations(){return _iterations;}
-	
-	public IEResult getBestSolution() {
-		//assert(_best != null) : "possibly called before search was run";
-		return _best;
-	}
-	
-	private boolean feasibleOrder(int[] order, double[] avgPos) {
-		//assert(false);
-		for(int i=0; i < order.length; i++){
-			int startPos = Math.min(i+1,_slots);
-			if(startPos < avgPos[order[i]]){
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	abstract protected boolean evalPerm(int[] perm);
+   public LDSearchSmart(int slots) {
+      _LDSQueue = new PriorityQueue<LDSPerm>();
+      _iterations = 0;
+      _distFactor = 10000;
+      _slots = slots;
+   }
+
+   public void search(int[] startPerm, double[] avgPos) {
+      _best = null;
+      _iterations = 0;
+      int Indexs = startPerm.length;
+      _LDSQueue.clear();
+      _LDSQueue.add(new LDSPerm(0, startPerm, new LinkedList<LDSSwap>()));
+
+      while (!_LDSQueue.isEmpty()) {
+         _iterations += 1;
+         LDSPerm perm = _LDSQueue.poll();
+
+         if (evalPerm(perm._perm)) {
+            return;
+         }
+
+         for (int i1 = 0; i1 < Indexs; i1++) {
+            for (int i2 = i1 + 1; i2 < Indexs; i2++) {
+               LDSSwap nextSwap = new LDSSwap(i1, i2);
+               if (!perm._swapped.contains(nextSwap)) {
+                  int[] nextPerm = new int[Indexs];
+                  for (int j = 0; j < Indexs; j++) {
+                     nextPerm[j] = perm._perm[j];
+                  }
+
+                  LinkedList<LDSSwap> nextSwapSet = new LinkedList<LDSSwap>();
+                  nextSwapSet.addAll(perm._swapped);
+                  nextSwapSet.add(new LDSSwap(i1, i2));
+
+                  int temp = nextPerm[i1];
+                  nextPerm[i1] = nextPerm[i2];
+                  nextPerm[i2] = temp;
+
+                  if (feasibleOrder(nextPerm, avgPos)) {
+
+//                     int delta = (int)(100*Math.abs(avgPos[nextPerm[i1]] - avgPos[nextPerm[i2]])); //needs a little more work, becouse avp array changes with swaps
+//
+//                     _LDSQueue.add(new LDSPerm(dsVal+_distFactor+delta, nextPerm, nextSwapSet));
+
+
+                     double[] avgPosClone = (double[]) avgPos.clone();
+                     double val = 0.0;
+                     for (LDSSwap swap : nextSwapSet) {
+                        int idx1 = swap.getFirstIdx();
+                        int idx2 = swap.getSecondIdx();
+                        val += Math.abs(avgPosClone[idx1] - avgPosClone[idx2]);
+
+                        double tmpAvg = avgPosClone[idx1];
+                        avgPosClone[idx1] = avgPosClone[idx2];
+                        avgPosClone[idx2] = tmpAvg;
+                     }
+
+                     int dsVal = (int) (1000 * val);
+
+                     _LDSQueue.add(new LDSPerm(dsVal + _distFactor, nextPerm, nextSwapSet));
+                  }
+               }
+            }
+         }
+      }
+   }
+
+   public int getIterations() {
+      return _iterations;
+   }
+
+   public IEResult getBestSolution() {
+      //assert(_best != null) : "possibly called before search was run";
+      return _best;
+   }
+
+   private boolean feasibleOrder(int[] order, double[] avgPos) {
+      //assert(false);
+      for (int i = 0; i < order.length; i++) {
+         int startPos = Math.min(i + 1, _slots);
+         if (startPos < avgPos[order[i]]) {
+            return false;
+         }
+      }
+      return true;
+   }
+
+   abstract protected boolean evalPerm(int[] perm);
 }
