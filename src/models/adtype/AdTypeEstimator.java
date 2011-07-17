8cb830c459bb6cc8d887d8fbf7f2755f6e1a8dc2
jberg <jberg@729a0f52-b9d0-41be-9dfd-8227063685e9>
2011-07-17 18:10:42 +0000

    Version that ran in the semi finals
    git-svn-id: svn://cs2955/var/svn/client/trunk@2266 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/models/adtype/AdTypeEstimator.java
+++ src/models/adtype/AdTypeEstimator.java
@@ -1,15 +1,14 @@
 package models.adtype;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
-
-import models.AbstractModel;
 import edu.umich.eecs.tac.props.Ad;
 import edu.umich.eecs.tac.props.Product;
 import edu.umich.eecs.tac.props.Query;
 import edu.umich.eecs.tac.props.QueryReport;
+import models.AbstractModel;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * This will simply track a histogram of adTypes shown by each advertiser.
@@ -20,95 +19,95 @@ import edu.umich.eecs.tac.props.QueryReport;
 public class AdTypeEstimator extends AbstractAdTypeEstimator {
 
 
-	Set<Query> _querySpace;
-	Set<String> _advertisersSet;
-	Set<Product> _products;
-	Set<Ad> _adSpace;
-	
-	//Keep track of how many times each (agent,query,adType) occurred
-	//String advertiser, Query query, Ad
-	HashMap<String, HashMap<Query, HashMap<Ad, Integer>>> numAppearances;
-	HashMap<String, HashMap<Query, Ad>> mostLikelyAd;
-	
-	
+   Set<Query> _querySpace;
+   Set<String> _advertisersSet;
+   Set<Product> _products;
+   Set<Ad> _adSpace;
 
-	public AdTypeEstimator(Set<Query> querySpace, Set<String> advertisersSet, Set<Product> products) {
-		this._querySpace = querySpace;
-		this._advertisersSet = advertisersSet;
-		this._products = products;
-		
-		//Create set of possible ads. //TODO: Does this already exist somewhere?
-		this._adSpace = new HashSet<Ad>();
-		_adSpace.add(new Ad()); //generic ad
-		for (Product product : _products) {
-			_adSpace.add(new Ad(product));
-		}
-		
-		//Initialize appearances map
-		numAppearances = new HashMap<String, HashMap<Query, HashMap<Ad, Integer>>>();
-		for (String advertiser : _advertisersSet) {
-			numAppearances.put(advertiser, new HashMap<Query, HashMap<Ad, Integer>>());
-			for (Query query : _querySpace) {
-				numAppearances.get(advertiser).put(query, new HashMap<Ad, Integer>());
-				for (Ad ad : _adSpace) {
-					numAppearances.get(advertiser).get(query).put(ad, 0);
-				}
-			}
-		}
-
-		
-		//Initialize most likely ad map
-		mostLikelyAd = new HashMap<String, HashMap<Query, Ad>>();
-		for (String advertiser : _advertisersSet) {
-			mostLikelyAd.put(advertiser, new HashMap<Query, Ad>());
-			for (Query query : _querySpace) {
-				//Have the most likely ad be generic by default
-				mostLikelyAd.get(advertiser).put(query, new Ad());
-			}
-		}		
-	}
+   //Keep track of how many times each (agent,query,adType) occurred
+   //String advertiser, Query query, Ad
+   HashMap<String, HashMap<Query, HashMap<Ad, Integer>>> numAppearances;
+   HashMap<String, HashMap<Query, Ad>> mostLikelyAd;
 
 
-	@Override
-	public Ad getAdTypeEstimate(Query q, String advertiser) {
-		// TODO Add try/catch in case a bad query or advertiser is given
-		Ad ad = mostLikelyAd.get(advertiser).get(q);
-		return ad;
-	}
 
-	@Override
-	public void updateModel(QueryReport queryReport) {
-		
-		//Update histogram
-		for (String advertiser : _advertisersSet) {
-			for (Query query : _querySpace) {
-				queryReport.getAd(query, advertiser);
-	            Ad ad = queryReport.getAd(query, advertiser);
-	            if (ad != null) {
-	               incrementNumAppearances(advertiser, query, ad);
-	            }
-			}
-		}
-		
-		//Decide which ads are most likely
-		for (String advertiser : _advertisersSet) {
-			for (Query query : _querySpace) {
-				Ad highestFrequencyAd = null;
-				int highestFrequency = -1;
-				
-				for (Ad ad : _adSpace) {
-					int frequency = numAppearances.get(advertiser).get(query).get(ad);
-					if (frequency > highestFrequency) {
-						highestFrequency = frequency;
-						highestFrequencyAd = ad;
-					}
-				}
-				
-				mostLikelyAd.get(advertiser).put(query, highestFrequencyAd);
-			}
-		}
-		
-		
+   public AdTypeEstimator(Set<Query> querySpace, Set<String> advertisersSet, Set<Product> products) {
+      this._querySpace = querySpace;
+      this._advertisersSet = advertisersSet;
+      this._products = products;
+
+      //Create set of possible ads. //TODO: Does this already exist somewhere?
+      this._adSpace = new HashSet<Ad>();
+      _adSpace.add(new Ad()); //generic ad
+      for (Product product : _products) {
+         _adSpace.add(new Ad(product));
+      }
+
+      //Initialize appearances map
+      numAppearances = new HashMap<String, HashMap<Query, HashMap<Ad, Integer>>>();
+      for (String advertiser : _advertisersSet) {
+         numAppearances.put(advertiser, new HashMap<Query, HashMap<Ad, Integer>>());
+         for (Query query : _querySpace) {
+            numAppearances.get(advertiser).put(query, new HashMap<Ad, Integer>());
+            for (Ad ad : _adSpace) {
+               numAppearances.get(advertiser).get(query).put(ad, 0);
+            }
+         }
+      }
+
+
+      //Initialize most likely ad map
+      mostLikelyAd = new HashMap<String, HashMap<Query, Ad>>();
+      for (String advertiser : _advertisersSet) {
+         mostLikelyAd.put(advertiser, new HashMap<Query, Ad>());
+         for (Query query : _querySpace) {
+            //Have the most likely ad be generic by default
+            mostLikelyAd.get(advertiser).put(query, new Ad());
+         }
+      }
+   }
+
+
+   @Override
+   public Ad getAdTypeEstimate(Query q, String advertiser) {
+      // TODO Add try/catch in case a bad query or advertiser is given
+      Ad ad = mostLikelyAd.get(advertiser).get(q);
+      return ad;
+   }
+
+   @Override
+   public void updateModel(QueryReport queryReport) {
+
+      //Update histogram
+      for (String advertiser : _advertisersSet) {
+         for (Query query : _querySpace) {
+            queryReport.getAd(query, advertiser);
+            Ad ad = queryReport.getAd(query, advertiser);
+            if (ad != null) {
+               incrementNumAppearances(advertiser, query, ad);
+            }
+         }
+      }
+
+      //Decide which ads are most likely
+      for (String advertiser : _advertisersSet) {
+         for (Query query : _querySpace) {
+            Ad highestFrequencyAd = null;
+            int highestFrequency = -1;
+
+            for (Ad ad : _adSpace) {
+               int frequency = numAppearances.get(advertiser).get(query).get(ad);
+               if (frequency > highestFrequency) {
+                  highestFrequency = frequency;
+                  highestFrequencyAd = ad;
+               }
+            }
+
+            mostLikelyAd.get(advertiser).put(query, highestFrequencyAd);
+         }
+      }
+
+
 //		//DEBUG
 //		//Print values
 //		System.out.println("Ad counts:");
@@ -129,21 +128,30 @@ public class AdTypeEstimator extends AbstractAdTypeEstimator {
 //			}
 //		}
 //		//END DEBUG
-		
-	}
+
+   }
 
 
-	
-	private void incrementNumAppearances(String advertiser, Query query, Ad ad) {
-		int previousVal = numAppearances.get(advertiser).get(query).get(ad);
-		numAppearances.get(advertiser).get(query).put(ad, previousVal+1);
-	}
+
+   private void incrementNumAppearances(String advertiser, Query query, Ad ad) {
+      try {
+         if(ad == null) {
+            ad = new Ad();
+         }
+         if(numAppearances.get(advertiser).get(query).get(ad) != null) {
+            int previousVal = numAppearances.get(advertiser).get(query).get(ad);
+            numAppearances.get(advertiser).get(query).put(ad, previousVal+1);
+         }
+      }
+      catch(Exception e) {
+         e.printStackTrace();
+      }
+   }
 
 
-	@Override
-	public AbstractModel getCopy() {
-		// TODO Auto-generated method stub
-		return new AdTypeEstimator(_querySpace, _advertisersSet, _products);
-	}
+   @Override
+   public AbstractModel getCopy() {
+      return new AdTypeEstimator(_querySpace, _advertisersSet, _products);
+   }
 
 }
