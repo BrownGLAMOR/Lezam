c2ba1d83554be1894f5eb502d4585072d727ad05
betsy <betsy@729a0f52-b9d0-41be-9dfd-8227063685e9>
2012-08-07 20:24:34 +0000

    Sam edits to make TestQA running
    git-svn-id: svn://cs2955/var/svn/client/trunk@2302 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/models/queryanalyzer/TestQA.java
+++ src/models/queryanalyzer/TestQA.java
@@ -8,34 +8,65 @@ import models.queryanalyzer.search.LDSearchIESmart;
 import models.queryanalyzer.util.LoadData;
 
 import java.util.Arrays;
-
+import java.io.*;
 public class TestQA {
+	private BufferedReader reader=null;
 	
-	public void run(String[] args) {
-		assert(args.length > 0);
-		String fileName = args[0];
-		System.out.println("Loading File: "+args[0]);
-		System.out.println("");
+	public TestQA(){
+		reader=new BufferedReader(new InputStreamReader(System.in));
+	}
+	public void run(String[] args,boolean readfromCommandLine) throws IOException{
+		String fileName="";
+		// Read from the commandLine
+		
+		try{
+		if(readfromCommandLine){
+			assert(args.length > 0);
+			fileName = args[0];
+			System.out.println("Loading File: "+args[0]);
+			System.out.println("");
+		}
+		// Input the filePath by user
+		else{
+			System.out.println("Please input file path:");
+			fileName = reader.readLine();
+			System.out.println("Loading File: "+fileName);
+			System.out.println("");
+			
+		}
+		}catch(IOException e){
+			System.out.println(e);
+		}
+		
 		QAData data = LoadData.LoadIt(fileName);
 		
 		System.out.println("All Data:");
 		System.out.println(data);
 		
+		// Why are we hard code here?
 		int advetiser = 3;
 		QAInstance inst = data.buildInstances(advetiser);
 		
 		System.out.println("Instance for "+advetiser+":");
 		System.out.println(inst);
 		
+		
 		int[] avgPosOrder = QAInstance.getAvgPosOrder(inst.getAvgPos());
+		
+
 		System.out.println("AvgPos order: "+Arrays.toString(avgPosOrder));
 		
+		System.out.println("passA");
+
 		ImpressionEstimatorSample ie = new ImpressionEstimatorSample(inst);
 
+		System.out.println("passB");
+
+		
 		LDSearchIESmart smartIESearcher = new LDSearchIESmart(10, ie);
 		smartIESearcher.search(avgPosOrder, inst.getAvgPos());
 		IEResult bestSol = smartIESearcher.getBestSolution();
-		
+
 		System.out.println("Smart Iterations: "+smartIESearcher.getIterations());
 		System.out.println("Best solution: "+Arrays.toString(bestSol.getSol()));
 		
@@ -51,6 +82,6 @@ public class TestQA {
 	}
 	
 	public static void main(String[] args) throws Exception {
-		new TestQA().run(args);
+		new TestQA().run(args,false);
 	}
 }
