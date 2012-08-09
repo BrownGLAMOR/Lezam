07846f06308edec58d158005014da86adbcc2665
betsy <betsy@729a0f52-b9d0-41be-9dfd-8227063685e9>
2012-08-09 17:30:52 +0000

    Sam edits to add 3 extra argument: sampledAveragePositions, impsDistMean, impsDistdev
    git-svn-id: svn://cs2955/var/svn/client/trunk@2320 729a0f52-b9d0-41be-9dfd-8227063685e9

--- src/models/queryanalyzer/ds/AdvertiserInfo.java
+++ src/models/queryanalyzer/ds/AdvertiserInfo.java
@@ -6,16 +6,25 @@ public class AdvertiserInfo {
 	public int impressions;
 	public double bid;
 	public double budget;
-	
-	public AdvertiserInfo(int id, double avgPos, int impressions, double bid, double budget){
+	public double sampledAveragePositions; 
+	public double impsDistMean; 
+	public double impsDistStdev; 
+
+	public AdvertiserInfo(int id, double avgPos, int impressions, double bid, double budget, 
+	double sampledAveragePositions, double impsDistMean, double impsDistStdev)
+	{
 		this.id = id;
 		this.avgPos = avgPos;
 		this.impressions = impressions;
 		this.bid = bid;
 		this.budget = budget;
+		this.sampledAveragePositions=sampledAveragePositions;
+		this.impsDistMean=impsDistMean;
+		this.impsDistStdev=impsDistStdev;
 	}
 	
 	public String toString() {
-		return id + " " + avgPos + " " + impressions + " " + bid + " " + budget;
+		return id + " " + avgPos + " " + impressions + " " + bid + " " + budget+" "+sampledAveragePositions
+		+" "+impsDistMean+" "+impsDistStdev;
 	}
 }
